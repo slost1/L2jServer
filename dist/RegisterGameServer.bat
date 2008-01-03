a6a17e25461610950f0fea6d9fe1d0e03d660419
DrLecter <DrLecter@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2008-01-03 02:22:03 +0000

    Luckily I don't do this to often :P
    git-svn-id: http://svn.l2jserver.com/trunk@1437 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- dist/RegisterGameServer.bat
+++
@@ -1,2 +0,0 @@
-@java -Djava.util.logging.config.file=console.cfg -cp ./../libs/*;l2jserver.jar net.sf.l2j.gsregistering.GameServerRegister
-@pause
--- dist/startSQLAccountManager.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -Djava.util.logging.config.file=console.cfg -cp ./../libs/*:l2jserver.jar net.sf.l2j.accountmanager.SQLAccountManager
