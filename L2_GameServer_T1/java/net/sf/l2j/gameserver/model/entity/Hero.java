fff58e0667ba0734310413626ad76c6342c09faf
DrLecter <DrLecter@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2008-01-10 06:47:46 +0000

    imports cleanup. tnx Ashitaka.
    git-svn-id: http://svn.l2jserver.com/trunk@1501 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer_T1/java/net/sf/l2j/gameserver/model/entity/Hero.java
+++ L2_GameServer_T1/java/net/sf/l2j/gameserver/model/entity/Hero.java
@@ -27,7 +27,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
