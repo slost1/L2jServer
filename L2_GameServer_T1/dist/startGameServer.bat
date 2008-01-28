2118993f0016193b95c3a745ecac93a9ffa5a455
Sami <Sami@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2008-01-28 15:31:27 +0000

    higher default memory settings
    git-svn-id: http://svn.l2jserver.com/trunk@1617 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer_T1/dist/startGameServer.bat
+++ L2_GameServer_T1/dist/startGameServer.bat
@@ -5,7 +5,7 @@ echo Starting L2J Game Server.
 echo.
 REM -------------------------------------
 REM Default parameters for a basic server.
-java -Xmx512m -cp ./../libs/*;l2jserver.jar net.sf.l2j.gameserver.GameServer
+java -Xmx768m -cp ./../libs/*;l2jserver.jar net.sf.l2j.gameserver.GameServer
 REM
 REM If you have a big server and lots of memory, you could experiment for example with
 REM java -server -Xmx1536m -Xms1024m -Xmn512m -XX:PermSize=256m -XX:SurvivorRatio=8 -Xnoclassgc -XX:+AggressiveOpts
