f8f681ce6d8f5edfe554295d59eb421a93bbc061
Fulminus <Fulminus@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2008-01-30 16:32:14 +0000

    Just formatting the code using eclipse's auto-formater (I plan to work on this code and changes will be more visible if the formatting is committed separately)
    git-svn-id: http://svn.l2jserver.com/trunk@1628 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer_T1/java/net/sf/l2j/gameserver/model/zone/type/L2BossZone.java
+++ L2_GameServer_T1/java/net/sf/l2j/gameserver/model/zone/type/L2BossZone.java
@@ -3,12 +3,12 @@
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *
+ * 
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -25,74 +25,78 @@ import net.sf.l2j.gameserver.model.zone.L2ZoneType;
 public class L2BossZone extends L2ZoneType
 {
     private String _zoneName;
+    
     private int _timeInvade;
-
+    
     public L2BossZone(int id)
     {
-	super(id);
+        super(id);
     }
-
+    
     @Override
     public void setParameter(String name, String value)
     {
-	if (name.equals("name"))
-	{
-	    _zoneName = value;
-	}
-
-	if (name.equals("InvadeTime"))
-	{
-	    _timeInvade = Integer.parseInt(value);
-	}
+        if (name.equals("name"))
+        {
+            _zoneName = value;
+        }
+        
+        if (name.equals("InvadeTime"))
+        {
+            _timeInvade = Integer.parseInt(value);
+        }
         else
-	{
-	    super.setParameter(name, value);
-	}
+        {
+            super.setParameter(name, value);
+        }
     }
-
+    
     @Override
     protected void onEnter(L2Character character)
     {
-	if (character instanceof L2PcInstance)
-	{
-	    if (((L2PcInstance) character).isGM())
-	    {
-		   ((L2PcInstance) character).sendMessage("You entered " + _zoneName);
-	    }
-	    if (!((L2PcInstance) character).isGM() && (System.currentTimeMillis() - ((L2PcInstance) character).getLastAccess() >= _timeInvade))
-	    {
-		   ((L2PcInstance) character).teleToLocation(MapRegionTable.TeleportWhereType.Town);
-	    }
-      }
+        if (character instanceof L2PcInstance)
+        {
+            if (((L2PcInstance) character).isGM())
+            {
+                ((L2PcInstance) character).sendMessage("You entered "
+                        + _zoneName);
+            }
+            if (!((L2PcInstance) character).isGM()
+                    && (System.currentTimeMillis()
+                            - ((L2PcInstance) character).getLastAccess() >= _timeInvade))
+            {
+                ((L2PcInstance) character).teleToLocation(MapRegionTable.TeleportWhereType.Town);
+            }
+        }
     }
-
+    
     @Override
     protected void onExit(L2Character character)
     {
-	if (character instanceof L2PcInstance)
-	{
-	    if (((L2PcInstance) character).isGM())
-	    {
-		((L2PcInstance) character).sendMessage("You left " + _zoneName);
-	    }
-	}
+        if (character instanceof L2PcInstance)
+        {
+            if (((L2PcInstance) character).isGM())
+            {
+                ((L2PcInstance) character).sendMessage("You left " + _zoneName);
+            }
+        }
     }
-
+    
     public String getZoneName()
     {
-	return _zoneName;
+        return _zoneName;
     }
-
+    
     public int getTimeInvade()
     {
-	return _timeInvade;
+        return _timeInvade;
     }
-
+    
     @Override
     protected void onDieInside(L2Character character)
     {
     }
-
+    
     @Override
     protected void onReviveInside(L2Character character)
     {
