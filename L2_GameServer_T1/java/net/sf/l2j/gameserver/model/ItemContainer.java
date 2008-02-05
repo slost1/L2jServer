7b0c6640f195e5356ba7a44598820b63458b81e4
KenM <KenM@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2008-02-05 06:00:06 +0000

    Fixed itens returning to original owner/place on some situations when using LazyItemsUpdate=true (#2026)
    git-svn-id: http://svn.l2jserver.com/trunk@1655 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer_T1/java/net/sf/l2j/gameserver/model/ItemContainer.java
+++ L2_GameServer_T1/java/net/sf/l2j/gameserver/model/ItemContainer.java
@@ -359,7 +359,7 @@ public abstract class ItemContainer
         	}
 
     		// Updates database
-    		sourceitem.updateDatabase();
+    		sourceitem.updateDatabase(true);
     		if (targetitem != sourceitem && targetitem != null) targetitem.updateDatabase();
     		if (sourceitem.isAugmented())
     			sourceitem.getAugmentation().removeBonus(actor);
