b64a73bc63533ae6df11890daf7708efb8b50d47
nBd <nBd@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2008-02-23 19:33:18 +0000

    Fix for invul SiegeHQ's
    git-svn-id: http://svn.l2jserver.com/trunk@1753 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer_T1/java/net/sf/l2j/gameserver/model/L2Character.java
+++ L2_GameServer_T1/java/net/sf/l2j/gameserver/model/L2Character.java
@@ -60,6 +60,7 @@ import net.sf.l2j.gameserver.model.actor.instance.L2PcInstance;
 import net.sf.l2j.gameserver.model.actor.instance.L2PetInstance;
 import net.sf.l2j.gameserver.model.actor.instance.L2PlayableInstance;
 import net.sf.l2j.gameserver.model.actor.instance.L2RiftInvaderInstance;
+import net.sf.l2j.gameserver.model.actor.instance.L2SiegeFlagInstance;
 import net.sf.l2j.gameserver.model.actor.instance.L2SiegeGuardInstance;
 import net.sf.l2j.gameserver.model.actor.instance.L2SiegeSummonInstance;
 import net.sf.l2j.gameserver.model.actor.instance.L2SummonInstance;
@@ -283,7 +284,8 @@ public abstract class L2Character extends L2Object
 			!(this instanceof L2ControlTowerInstance) && !(this instanceof L2DoorInstance) &&
 			!(this instanceof L2TrapInstance) && !(this instanceof L2FriendlyMobInstance) &&
 			!(this instanceof L2DecoyInstance) && !(this instanceof L2SiegeSummonInstance) &&
-			!(this instanceof L2PetInstance) && !(this instanceof L2SummonInstance))
+			!(this instanceof L2PetInstance) && !(this instanceof L2SummonInstance) &&
+			!(this instanceof L2SiegeFlagInstance))
 			setIsInvul(true);
 	}
 
