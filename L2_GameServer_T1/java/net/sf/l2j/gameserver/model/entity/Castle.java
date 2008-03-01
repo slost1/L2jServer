6c12c83af2b3c36b0e5b6d829b8da3b096f24049
Ahmed <Ahmed@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2008-03-01 18:44:27 +0000

    - Replacing: new Accouncements().announceToAll("blabala"); with: Accouncements.getInstance().announceToAll("blabala"); - Adding code to play siege victory sound for clan who takes a castle. Thanks FBIagent
    git-svn-id: http://svn.l2jserver.com/trunk@1763 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer_T1/java/net/sf/l2j/gameserver/model/entity/Castle.java
+++ L2_GameServer_T1/java/net/sf/l2j/gameserver/model/entity/Castle.java
@@ -42,6 +42,7 @@ import net.sf.l2j.gameserver.model.actor.instance.L2DoorInstance;
 import net.sf.l2j.gameserver.model.actor.instance.L2PcInstance;
 import net.sf.l2j.gameserver.model.zone.type.L2CastleZone;
 import net.sf.l2j.gameserver.serverpackets.PledgeShowInfoUpdate;
+import net.sf.l2j.gameserver.serverpackets.PlaySound;
 
 public class Castle
 {
@@ -276,7 +277,7 @@ public class Castle
 					}
 				}
 				oldOwner.setHasCastle(0);												// Unset has castle flag for old owner
-        		new Announcements().announceToAll(oldOwner.getName() + " has lost " + getName() + " castle!");
+        		Announcements.getInstance().announceToAll(oldOwner.getName() + " has lost " + getName() + " castle!");
 			}
 	    }
 
@@ -298,7 +299,7 @@ public class Castle
 				CastleManager.getInstance().removeCirclet(_formerOwner,getCastleId());
 			}
 			clan.setHasCastle(0);
-			new Announcements().announceToAll(clan.getName() + " has lost " +getName() + " castle");
+			Announcements.getInstance().announceToAll(clan.getName() + " has lost " +getName() + " castle");
 			clan.broadcastToOnlineMembers(new PledgeShowInfoUpdate(clan));
 		}
 
@@ -614,9 +615,9 @@ public class Castle
             if (clan != null)
             {
     		    clan.setHasCastle(getCastleId()); // Set has castle flag for new owner
-    		    new Announcements().announceToAll(clan.getName() + " has taken " + getName() + " castle!");
+    		    Announcements.getInstance().announceToAll(clan.getName() + " has taken " + getName() + " castle!");
     		    clan.broadcastToOnlineMembers(new PledgeShowInfoUpdate(clan));
-
+   		    clan.broadcastToOnlineMembers(new PlaySound(1, "Siege_Victory", 0, 0, 0, 0, 0));
     		    ThreadPoolManager.getInstance().scheduleGeneral(new CastleUpdater(clan, 1), 3600000);	// Schedule owner tasks to start running
             }
         }
