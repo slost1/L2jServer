088bcb325d3516bcd0ad0d173392f1a82e214322
Ahmed <Ahmed@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2008-03-12 21:02:36 +0000

    Cleaning, lets not add code, and comment old code, simply remove it. We can check the repositories browse features if we want to reference something in the past. Adding useless comments as this one should be avoided. Thanks Emperorc
    git-svn-id: http://svn.l2jserver.com/trunk@1810 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer_T1/java/net/sf/l2j/gameserver/model/actor/instance/L2NpcInstance.java
+++ L2_GameServer_T1/java/net/sf/l2j/gameserver/model/actor/instance/L2NpcInstance.java
@@ -1238,7 +1238,6 @@ public class L2NpcInstance extends L2Character
         Quest q = QuestManager.getInstance().getQuest(questId);
 
         if (player.getWeightPenalty()>=3 || player.getInventoryLimit()*0.8 <= player.getInventory().getSize())
-        		//&& q.getQuestIntId() >= 1 && q.getQuestIntId() < 1000 )
         {	
             player.sendPacket(new SystemMessage(SystemMessageId.INVENTORY_LESS_THAN_80_PERCENT));
             return;
