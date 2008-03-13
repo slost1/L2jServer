d21d453e204498de2fff585ac8cda221f74dca56
nBd <nBd@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2008-03-13 19:41:30 +0000

    Fixes MP consume for "stance" type toggle skills based on player level. tnx slyce
    git-svn-id: http://svn.l2jserver.com/trunk@1814 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

---
+++ L2_GameServer_T1/java/net/sf/l2j/gameserver/skills/effects/EffectMpConsumePerLevel.java
@@ -0,0 +1,55 @@
+/*
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sf.l2j.gameserver.skills.effects;
+
+import net.sf.l2j.gameserver.model.L2Effect;
+import net.sf.l2j.gameserver.network.SystemMessageId;
+import net.sf.l2j.gameserver.serverpackets.SystemMessage;
+import net.sf.l2j.gameserver.skills.Env;
+
+
+class EffectMpConsumePerLevel extends L2Effect
+{
+	public EffectMpConsumePerLevel(Env env, EffectTemplate template)
+	{
+		super(env, template);
+	}
+
+	@Override
+	public EffectType getEffectType()
+	{
+		return EffectType.MP_CONSUME_PER_LEVEL ;
+	}
+
+	@Override
+	public boolean onActionTime()
+	{
+		if(getEffected().isDead())
+			return false;
+
+		double base = calc();
+		double consume = (getEffected().getLevel() - 1)/7.5*base*getPeriod();
+
+		if(consume > getEffected().getCurrentMp())
+		{
+			SystemMessage sm = new SystemMessage(SystemMessageId.SKILL_REMOVED_DUE_LACK_MP);
+			getEffected().sendPacket(sm);
+			return false;
+		}
+
+		getEffected().reduceCurrentMp(consume);
+		return true;
+	}
+}
