516aec7678f6d31fbd7165d53fff0259c933cea5
DrLecter <DrLecter@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2008-03-13 11:26:24 +0000

    Fix #2228
    git-svn-id: http://svn.l2jserver.com/trunk@1813 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer_T1/java/net/sf/l2j/gameserver/handler/usercommandhandlers/Escape.java
+++ L2_GameServer_T1/java/net/sf/l2j/gameserver/handler/usercommandhandlers/Escape.java
@@ -23,11 +23,9 @@ import net.sf.l2j.gameserver.handler.IUserCommandHandler;
 import net.sf.l2j.gameserver.instancemanager.GrandBossManager;
 import net.sf.l2j.gameserver.model.actor.instance.L2PcInstance;
 import net.sf.l2j.gameserver.model.entity.TvTEvent;
-import net.sf.l2j.gameserver.network.SystemMessageId;
 import net.sf.l2j.gameserver.serverpackets.ActionFailed;
 import net.sf.l2j.gameserver.serverpackets.MagicSkillUse;
 import net.sf.l2j.gameserver.serverpackets.SetupGauge;
-import net.sf.l2j.gameserver.serverpackets.SystemMessage;
 import net.sf.l2j.gameserver.util.Broadcast;
 
 /**
