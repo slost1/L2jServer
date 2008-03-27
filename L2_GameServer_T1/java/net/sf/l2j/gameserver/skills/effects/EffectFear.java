a3896b97a3280971f8fd5f574632f2de821264b3
Ahmed <Ahmed@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2008-03-27 19:06:59 +0000

    Some Fear skill exceptions, thanks Aikimaniac
    git-svn-id: http://svn.l2jserver.com/trunk@1858 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer_T1/java/net/sf/l2j/gameserver/skills/effects/EffectFear.java
+++ L2_GameServer_T1/java/net/sf/l2j/gameserver/skills/effects/EffectFear.java
@@ -66,8 +66,8 @@ final class EffectFear extends L2Effect
 	@Override
 	public boolean onActionTime()
 	{
-		// Fear skills cannot be used l2pcinstance to l2pcinstance. Heroic Dread, Curse: Fear, Fear and Horror are the exceptions.
-		if(getEffected() instanceof L2PcInstance && getEffector() instanceof L2PcInstance && getSkill().getId() != 1376 && getSkill().getId() != 1169 && getSkill().getId() != 65 && getSkill().getId() != 1092) return false;
+		// Fear skills cannot be used l2pcinstance to l2pcinstance. Heroic Dread, Curse: Fear, Fear, Horror, Sword Symphony, Word of Fear and Mass Curse Fear are the exceptions.
+		if(getEffected() instanceof L2PcInstance && getEffector() instanceof L2PcInstance && getSkill().getId() != 1376 && getSkill().getId() != 1169 && getSkill().getId() != 65 && getSkill().getId() != 1092 && getSkill().getId() != 98 && getSkill().getId() != 1272 && getSkill().getId() != 1381) return false;
 		if(getEffected() instanceof L2FolkInstance) return false;
 		if(getEffected() instanceof L2SiegeGuardInstance) return false;
 		// Fear skills cannot be used on Headquarters Flag.
