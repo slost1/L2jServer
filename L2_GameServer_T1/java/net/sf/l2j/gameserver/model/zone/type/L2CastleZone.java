c35325b114af93411a191a59b8372990cfb9a482
Sami <Sami@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2008-04-05 14:27:30 +0000

    fix for banish players by Kerberos
    git-svn-id: http://svn.l2jserver.com/trunk@1913 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer_T1/java/net/sf/l2j/gameserver/model/zone/type/L2CastleZone.java
+++ L2_GameServer_T1/java/net/sf/l2j/gameserver/model/zone/type/L2CastleZone.java
@@ -153,8 +153,8 @@ public class L2CastleZone extends L2ZoneType
 	{
 		for (L2Character temp : _characterList.values())
 		{
-			if(!(temp instanceof L2PcInstance)) continue;
-			if (((L2PcInstance)temp).getClanId() == owningClanId) continue;
+			if(!(temp instanceof L2PcInstance)) return;
+			if (((L2PcInstance)temp).getClanId() != owningClanId) continue;
 
 			((L2PcInstance)temp).teleToLocation(MapRegionTable.TeleportWhereType.Town);
 		}
