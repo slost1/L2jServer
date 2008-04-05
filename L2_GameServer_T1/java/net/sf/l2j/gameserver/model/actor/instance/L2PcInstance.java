78f5e19d23b5e73afeea5a3fe5d1e1d4ac5cab42
Sami <Sami@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2008-04-05 04:54:01 +0000

    fixes #2309, mount when transformed
    git-svn-id: http://svn.l2jserver.com/trunk@1911 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer_T1/java/net/sf/l2j/gameserver/model/actor/instance/L2PcInstance.java
+++ L2_GameServer_T1/java/net/sf/l2j/gameserver/model/actor/instance/L2PcInstance.java
@@ -5713,10 +5713,10 @@ public final class L2PcInstance extends L2PlayableInstance
     
     public boolean mount(L2Summon pet)
     {
-        if (!this.disarmWeapons())
-        {
+        if (!disarmWeapons())
             return false;
-        }
+        if (isTransformed())
+        	return false;
         
         Ride mount = new Ride(this, true, pet.getTemplate().npcId);
         this.setMount(pet.getTemplate().npcId, mount.getMountType());
@@ -5728,16 +5728,15 @@ public final class L2PcInstance extends L2PlayableInstance
         
         pet.unSummon(this);
         
-        
         return true;
     }
     
     public boolean mount(int npcId, int controlItemObjId)
     {
-        if (!this.disarmWeapons())
-        {
+        if (!disarmWeapons())
             return false;
-        }
+        if (isTransformed())
+        	return false;
         
         Ride mount = new Ride(this, true, npcId);
         this.setMount(npcId, mount.getMountType());
