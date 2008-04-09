280de60d1ecfa11705a882f5f47c9553ec8b429f
Ahmed <Ahmed@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2008-04-09 06:13:31 +0000

    I hate looking in a file about 1000000 lines long to find all "non-consecutive commands" for some GM's logs, so I've changed the way its done so that each GM gets his own log file which will help isolate some tracking. You must have the GMAudit set to true in the General.properties file for this to take effect. You fill find the logs now in the /logs/GMAudit/gmName.txt, where gmName is the GM's name hehehe.... I hope to increase GM tracking so Admins can see GMs breath in game (xD joken)..... Thank you for testing this ShanSoft.
    git-svn-id: http://svn.l2jserver.com/trunk@1934 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer_T1/java/net/sf/l2j/gameserver/model/GMAudit.java
+++ L2_GameServer_T1/java/net/sf/l2j/gameserver/model/GMAudit.java
@@ -14,29 +14,50 @@
  */
 package net.sf.l2j.gameserver.model;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import net.sf.l2j.Config;
+import net.sf.l2j.gameserver.lib.Log;
 
-public class GMAudit {
-	private static final Logger _log = Logger.getLogger("gmaudit");
-
-	public static void auditGMAction(String gmName, String action, String target, String params){
-		if (Config.GMAUDIT){
-            String today;
-            SimpleDateFormat formatter;
-            formatter = new SimpleDateFormat("dd/MM/yyyy H:mm:ss");
-            today = formatter.format(new Date());
-
-			_log.log(Level.INFO, today + ">" + gmName + ">" + action + ">" +target + ">" + params);
+public class GMAudit
+{
+	
+	private static final Logger _log = Logger.getLogger(Log.class.getName());
+	
+	public static void auditGMAction(String gmName, String action, String target, String params)
+	{
+		if (Config.GMAUDIT)
+		{
+			new File("log/GMAudit").mkdirs();
+			
+			SimpleDateFormat formatter;
+			formatter = new SimpleDateFormat("dd/MM/yyyy H:mm:ss");
+			String today = formatter.format(new Date());
+			
+			try
+			{
+				File file = new File("log/GMAudit/" + gmName + ".txt");
+				FileWriter save = new FileWriter(file, true);
+				
+				String out = (today + ">" + gmName + ">" + action + ">" + target + ">" + params + "\n");
+				
+				save.write(out);
+				save.flush();
+				save.close();
+				
+				save = null;
+				file = null;
+			}
+			catch (IOException e)
+			{
+				_log.log(Level.SEVERE, "GMAudit for GM " + gmName +" could not be saved: ", e);
+			}
 		}
 	}
 }
-
-
-
-
-
