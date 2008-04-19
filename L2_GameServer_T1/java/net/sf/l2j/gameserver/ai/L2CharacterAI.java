402410a472bd11fe168e5add420829e3a57f3e23
Ahmed <Ahmed@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2008-04-19 16:43:59 +0000

    Adding Light Worm restirction, thanks Loup_Solitaire.
    git-svn-id: http://svn.l2jserver.com/trunk@1987 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer_T1/java/net/sf/l2j/gameserver/ai/L2CharacterAI.java
+++ L2_GameServer_T1/java/net/sf/l2j/gameserver/ai/L2CharacterAI.java
@@ -1157,9 +1157,18 @@ public class L2CharacterAI extends AbstractAI
             // water movement analysis
             if (_actor instanceof L2NpcInstance)
             {
-                // well, lol..
-                if (((L2NpcInstance)_actor).getNpcId() == 20314) // great white shark
-                    cannotMoveOnLand = true;
+            	int npcId = ((L2NpcInstance)_actor).getNpcId();
+            	
+            	switch (npcId)
+            	{
+            		case 20314:
+            		case 20849:
+            			cannotMoveOnLand = true;
+            			break;
+            		default:
+            			cannotMoveOnLand = false;
+            			break;
+            	}
             }
             // skill analysis
             for (L2Skill sk : _actor.getAllSkills())
