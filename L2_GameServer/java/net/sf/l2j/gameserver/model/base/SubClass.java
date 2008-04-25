4528a52404466339b7b97c26882f4c556d6b50a9
nBd <nBd@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2008-04-25 23:45:35 +0000

    Subclass should be possible to go to 80 and 100%
    git-svn-id: http://svn.l2jserver.com/trunk@2035 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer/java/net/sf/l2j/gameserver/model/base/SubClass.java
+++ L2_GameServer/java/net/sf/l2j/gameserver/model/base/SubClass.java
@@ -88,8 +88,8 @@ public final class SubClass
 
     public void setExp(long expValue)
     {
-		if (expValue > Experience.LEVEL[80])
-			expValue = Experience.LEVEL[80];
+		if (expValue > (Experience.LEVEL[81] - 1))
+			expValue = (Experience.LEVEL[81] - 1);
 
         _exp = expValue;
     }
