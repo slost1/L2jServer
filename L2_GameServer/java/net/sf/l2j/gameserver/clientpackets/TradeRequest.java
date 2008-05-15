0df424f8bcdb3e13f16c1637c051a4f37c3f4209
durgus <durgus@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2008-05-15 23:23:46 +0000

    Fix for trade disabled bug
    git-svn-id: http://svn.l2jserver.com/trunk@2100 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer/java/net/sf/l2j/gameserver/clientpackets/TradeRequest.java
+++ L2_GameServer/java/net/sf/l2j/gameserver/clientpackets/TradeRequest.java
@@ -50,7 +50,7 @@ public final class TradeRequest extends L2GameClientPacket
 		L2PcInstance player = getClient().getActiveChar();
         if (player == null) return;
 
-        if (player.getAccessLevel().allowTransaction())
+        if (!player.getAccessLevel().allowTransaction())
         {
         	player.sendMessage("Transactions are disable for your Access Level");
             sendPacket(ActionFailed.STATIC_PACKET);
