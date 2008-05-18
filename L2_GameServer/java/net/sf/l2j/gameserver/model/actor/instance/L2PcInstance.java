9c6649ddd366e7880dee330ffcb1e5450201558a
Sami <Sami@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2008-05-18 19:51:25 +0000

    Death penalty fix tipped by Aikimaniac: The clan which declared war loses 1/4 of normal exp lost and the clan who has not accepted the war loses normal exp.
    git-svn-id: http://svn.l2jserver.com/trunk@2122 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer/java/net/sf/l2j/gameserver/model/actor/instance/L2PcInstance.java
+++ L2_GameServer/java/net/sf/l2j/gameserver/model/actor/instance/L2PcInstance.java
@@ -4698,8 +4698,10 @@ public final class L2PcInstance extends L2PlayableInstance
 						{
 							// Reduce the Experience of the L2PcInstance in function of the calculated Death Penalty
 							// NOTE: deathPenalty +- Exp will update karma
+							// Penalty is lower if the player is at war with the pk (war has to be declared)
 							if (getSkillLevel(L2Skill.SKILL_LUCKY) < 0 || getStat().getLevel() > 9)
-								deathPenalty((pk != null && getClan() != null && pk.getClan() != null && pk.getClan().isAtWarWith(getClanId())));
+								deathPenalty(pk != null && getClan() != null && getClan().isAtWarWith(pk.getClanId()));										
+
 						} else
 						{
 							onDieUpdateKarma(); // Update karma if delevel is not allowed
