460361b271e55fd4bbff37b24e59dd715cbcd2c3
Julian <Julian@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2008-06-04 16:29:59 +0000

    Deadlock fix
    git-svn-id: http://svn.l2jserver.com/trunk@2171 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer/java/net/sf/l2j/gameserver/model/actor/status/CharStatus.java
+++ L2_GameServer/java/net/sf/l2j/gameserver/model/actor/status/CharStatus.java
@@ -393,10 +393,11 @@ public class CharStatus
     public final void setCurrentHp(double newHp, boolean broadcastPacket)
     {
         // Get the Max HP of the L2Character
-        double maxHp = getActiveChar().getStat().getMaxHp();
+        double maxHp;
         
         synchronized (this)
         {
+        	maxHp = getActiveChar().getStat().getMaxHp();
             if (getActiveChar().isDead()) return;
         	if (newHp >= maxHp)
             {
