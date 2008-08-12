9e3483efc4630fd5e29466e558df1897e98ffc00
nBd <nBd@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2008-08-12 11:14:15 +0000

    Fix for Primeval Crystals - tnx Gigiikun
    git-svn-id: http://svn.l2jserver.com/trunk@2337 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer/java/net/sf/l2j/gameserver/handler/itemhandlers/Scrolls.java
+++ L2_GameServer/java/net/sf/l2j/gameserver/handler/itemhandlers/Scrolls.java
@@ -140,11 +140,13 @@ public class Scrolls implements IItemHandler
 	   		return;
 	   	}
         else if (itemId == 9897||itemId>=10131 &&itemId <=10138||itemId == 10151||itemId == 10274) //transformation scrolls
+        {
             if (activeChar.isMounted() || activeChar.getPet() != null || activeChar.isTransformed())
             {
                 activeChar.sendPacket(new SystemMessage(SystemMessageId.S1_CANNOT_BE_USED).addItemName(item));
                 return;
             }
+        }
 	   	else if (itemId >= 8954 && itemId <= 8956)
 	   	{
 	   		if (activeChar.getLevel() < 76)
