719ba4ed2c9ef540b1c3466346d87adf33b1d1f6
GodKratos <GodKratos@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2009-03-01 23:59:27 +0000

    Allow targeting while in shop mode. Thx kapitann22 :)
    git-svn-id: http://svn.l2jserver.com/trunk@2807 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer/java/net/sf/l2j/gameserver/network/clientpackets/Action.java
+++ L2_GameServer/java/net/sf/l2j/gameserver/network/clientpackets/Action.java
@@ -94,7 +94,7 @@ public final class Action extends L2GameClientPacket
 		}
 		
 		// Check if the target is valid, if the player haven't a shop or isn't the requester of a transaction (ex : FriendInvite, JoinAlly, JoinParty...)
-		if (activeChar.getPrivateStoreType() == 0 && activeChar.getActiveRequester() == null)
+		if (activeChar.getActiveRequester() == null)
 		{
 			switch (_actionId)
 			{
