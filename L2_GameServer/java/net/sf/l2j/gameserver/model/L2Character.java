51609fb821960b270e9b448f7178c392e19f87fa
GodKratos <GodKratos@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2009-03-02 00:13:00 +0000

    Small message update. Thx exz & Charus :)
    git-svn-id: http://svn.l2jserver.com/trunk@2808 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer/java/net/sf/l2j/gameserver/model/L2Character.java
+++ L2_GameServer/java/net/sf/l2j/gameserver/model/L2Character.java
@@ -1881,7 +1881,7 @@ public abstract class L2Character extends L2Object
             	else
                 {
             		// Send a System Message to the caster
-            		sendPacket(new SystemMessage(SystemMessageId.NOT_ENOUGH_ITEMS));
+            		sendPacket(new SystemMessage(SystemMessageId.THERE_ARE_NOT_ENOUGH_NECESSARY_ITEMS_TO_USE_THE_SKILL));
             		return false;
                 }
             }
