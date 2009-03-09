f6e5fd401ebe11d7a206a986fc51e542fd2e6721
DrHouse <DrHouse@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2009-03-09 17:37:06 +0000

    Simple way to enable logs while shutting down and saving data. Thanks Janiii
    git-svn-id: http://svn.l2jserver.com/trunk@2820 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer/dist/startGameServer.bat
+++ L2_GameServer/dist/startGameServer.bat
@@ -9,6 +9,9 @@ java -Xmx1024m -cp ./../libs/*;l2jserver.jar net.sf.l2j.gameserver.GameServer
 REM
 REM If you have a big server and lots of memory, you could experiment for example with
 REM java -server -Xmx1536m -Xms1024m -Xmn512m -XX:PermSize=256m -XX:SurvivorRatio=8 -Xnoclassgc -XX:+AggressiveOpts
+REM If you are having troubles on server shutdown (saving data),
+REM add this to startup paramethers: -Djava.util.logging.manager=net.sf.l2j.L2LogManager. Example:
+REM java -Djava.util.logging.manager=net.sf.l2j.util.L2LogManager -Xmx1024m -cp ./../libs/*;l2jserver.jar net.sf.l2j.gameserver.GameServer
 REM -------------------------------------
 if ERRORLEVEL 2 goto restart
 if ERRORLEVEL 1 goto error
