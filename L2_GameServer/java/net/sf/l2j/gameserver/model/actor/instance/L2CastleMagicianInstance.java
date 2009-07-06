b433840d3ba321832b7d451ca774ba8504fa5e58
_DS_ <_DS_@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2009-07-06 09:48:41 +0000

    Clan Gate skill implementation, thanks ZaKaX. Required DP update.
    git-svn-id: http://svn.l2jserver.com/trunk@3271 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer/java/net/sf/l2j/gameserver/model/actor/instance/L2CastleMagicianInstance.java
+++ L2_GameServer/java/net/sf/l2j/gameserver/model/actor/instance/L2CastleMagicianInstance.java
@@ -14,13 +14,19 @@
  */
 package net.sf.l2j.gameserver.model.actor.instance;
 
+import net.sf.l2j.Config;
+import net.sf.l2j.gameserver.SevenSigns;
 import net.sf.l2j.gameserver.network.serverpackets.ActionFailed;
 import net.sf.l2j.gameserver.network.serverpackets.NpcHtmlMessage;
 import net.sf.l2j.gameserver.templates.chars.L2NpcTemplate;
+import net.sf.l2j.gameserver.templates.skills.L2EffectType;
+import net.sf.l2j.gameserver.model.actor.L2Character;
+import net.sf.l2j.gameserver.model.entity.TvTEvent;
+import net.sf.l2j.gameserver.instancemanager.InstanceManager;
 
 
 /**
- * @author  Kerberos
+ * @author Kerberos | ZaKaX
  */
 
 public class L2CastleMagicianInstance extends L2NpcInstance
@@ -38,12 +44,14 @@ public class L2CastleMagicianInstance extends L2NpcInstance
 	}
 
 	@Override
-	public void showChatWindow(L2PcInstance player, int val) {
+	public void showChatWindow(L2PcInstance player, int val)
+	{
 		player.sendPacket( ActionFailed.STATIC_PACKET );
 		String filename = "data/html/castlemagician/magician-no.htm";
 
 		int condition = validateCondition(player);
-		if (condition > COND_ALL_FALSE) {
+		if (condition > COND_ALL_FALSE)
+		{
 			if (condition == COND_BUSY_BECAUSE_OF_SIEGE)
 				filename = "data/html/castlemagician/magician-busy.htm"; // Busy because of siege
 			else if (condition == COND_OWNER)                                    // Clan owns castle
@@ -63,7 +71,8 @@ public class L2CastleMagicianInstance extends L2NpcInstance
 	}
 
 	@Override
-	public void onBypassFeedback(L2PcInstance player, String command) {
+	public void onBypassFeedback(L2PcInstance player, String command)
+	{
 		if (command.startsWith("Chat"))
 		{
 			int val = 0;
@@ -75,13 +84,34 @@ public class L2CastleMagicianInstance extends L2NpcInstance
 			catch (NumberFormatException nfe){}
 			showChatWindow(player, val);
 		}
+		else if (command.equals("gotoleader"))
+		{
+			if (player.getClan() != null)
+			{
+				L2PcInstance clanLeader = player.getClan().getLeader().getPlayerInstance();
+				if (clanLeader == null)
+					return;
+
+				if (clanLeader.getFirstEffect(L2EffectType.CLAN_GATE) != null)
+				{
+					if (!validateGateCondition(clanLeader, player))
+						return;
+
+					player.sendMessage("You will be teleported throught the Clan Gate now.");
+					player.teleToLocation(clanLeader.getX(), clanLeader.getY(), clanLeader.getZ(), false);
+					return;
+				}
+				player.sendMessage("I cannot find the lord's signal right now, so it is impossible to cast the spell.");
+			}
+		}
 		else
 			super.onBypassFeedback(player, command);
 	}
 
 	protected int validateCondition(L2PcInstance player)
 	{
-		if (player.isGM()) return COND_OWNER;
+		if (player.isGM())
+			return COND_OWNER;
 		if (getCastle() != null && getCastle().getCastleId() > 0)
 		{
 			if (player.getClan() != null)
@@ -93,5 +123,106 @@ public class L2CastleMagicianInstance extends L2NpcInstance
 			}
 		}
 		return COND_ALL_FALSE;
+	}
+
+	private static final boolean validateGateCondition(L2PcInstance clanLeader, L2PcInstance player)
+	{
+		if (clanLeader.isAlikeDead())
+		{
+			// Need retail message if there's one.
+			player.sendMessage("Couldn't teleport to clan leader. The requirements was not meet.");
+			return false;
+		}
+
+		if (clanLeader.isInStoreMode())
+		{
+			// Need retail message if there's one.
+			player.sendMessage("Couldn't teleport to clan leader. The requirements was not meet.");
+			return false;
+		}
+
+		if (clanLeader.isRooted() || clanLeader.isInCombat())
+		{
+			// Need retail message if there's one.
+			player.sendMessage("Couldn't teleport to clan leader. The requirements was not meet.");
+			return false;
+		}
+
+		if (clanLeader.isInOlympiadMode())
+		{
+			// Need retail message if there's one.
+			player.sendMessage("Couldn't teleport to clan leader. The requirements was not meet.");
+			return false;
+		}
+
+		if (clanLeader.isFestivalParticipant())
+		{
+			// Need retail message if there's one.
+			player.sendMessage("Couldn't teleport to clan leader. The requirements was not meet.");
+			return false;
+		}
+
+		if (clanLeader.inObserverMode())
+		{
+			// Need retail message if there's one.
+			player.sendMessage("Couldn't teleport to clan leader. The requirements was not meet.");
+			return false;
+		}
+
+		if (clanLeader.isInsideZone(L2Character.ZONE_NOSUMMONFRIEND))
+		{
+			// Need retail message if there's one.
+			player.sendMessage("Couldn't teleport to clan leader. The requirements was not meet.");
+			return false;
+		}
+
+		if (clanLeader.getInstanceId() > 0)
+		{
+			if (!Config.ALLOW_SUMMON_TO_INSTANCE
+					|| InstanceManager.getInstance().getInstance(player.getInstanceId()).isSummonAllowed())
+			{
+				// Need retail message if there's one.
+				player.sendMessage("Couldn't teleport to clan leader. The requirements was not meet.");
+				return false;
+			}
+		}
+
+		if (player.isIn7sDungeon())
+		{
+			final int targetCabal = SevenSigns.getInstance().getPlayerCabal(clanLeader);
+			if (SevenSigns.getInstance().isSealValidationPeriod())
+			{
+				if (targetCabal != SevenSigns.getInstance().getCabalHighestScore())
+				{
+					// Need retail message if there's one.
+					player.sendMessage("Couldn't teleport to clan leader. The requirements was not meet.");
+					return false;
+				}
+			}
+			else
+			{
+				if (targetCabal == SevenSigns.CABAL_NULL)
+				{
+					// Need retail message if there's one.
+					player.sendMessage("Couldn't teleport to clan leader. The requirements was not meet.");
+					return false;
+				}
+			}
+		}
+
+		if (!TvTEvent.onEscapeUse(player.getObjectId()))
+		{
+			player.sendMessage("You on TvT Event, teleporting disabled.");
+			return false;
+		}
+
+		if (!TvTEvent.onEscapeUse(clanLeader.getObjectId()))
+		{
+			// Need retail message if there's one.
+			player.sendMessage("Couldn't teleport to clan leader. The requirements was not meet.");
+			return false;
+		}
+
+		return true;
 	}
 }
