4e8c4fad9dd8b190e62cbe4576ad812cf3781f49
janiii <janiii@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2009-07-07 06:51:46 +0000

    added ids of the new mounts
    git-svn-id: http://svn.l2jserver.com/trunk@3275 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer/java/net/sf/l2j/gameserver/network/serverpackets/Ride.java
+++ L2_GameServer/java/net/sf/l2j/gameserver/network/serverpackets/Ride.java
@@ -18,85 +18,86 @@ import net.sf.l2j.gameserver.model.actor.instance.L2PcInstance;
 
 public final class Ride extends L2GameServerPacket
 {
-    private static final String _S__8c_Ride = "[S] 8c Ride";
-    public static final int ACTION_MOUNT = 1;
-    public static final int ACTION_DISMOUNT = 0;
-    private final int _id;
-    private final int _bRide;
-    private final int _rideType;
-    private final int _rideClassID;
-    private final int _x, _y, _z;
-
-    public Ride(L2PcInstance cha, boolean mount, int rideClassId)
-    {
-        _id = cha.getObjectId();
-        _bRide = mount ? 1 : 0;
-        _rideClassID = rideClassId + 1000000; // npcID
-
-        _x = cha.getX();
-        _y = cha.getY();
-        _z = cha.getZ();
-        
-        switch(rideClassId)
-        {
-            case 0: // dismount
-                _rideType = 0;
-                break;
-            case 12526: // Wind
-            case 12527: // Star
-            case 12528: // Twilight
-            case 16038: // red strider of wind
+	private static final String _S__8c_Ride = "[S] 8c Ride";
+	public static final int ACTION_MOUNT = 1;
+	public static final int ACTION_DISMOUNT = 0;
+	private final int _id;
+	private final int _bRide;
+	private final int _rideType;
+	private final int _rideClassID;
+	private final int _x, _y, _z;
+	
+	public Ride(L2PcInstance cha, boolean mount, int rideClassId)
+	{
+		_id = cha.getObjectId();
+		_bRide = mount ? 1 : 0;
+		_rideClassID = rideClassId + 1000000; // npcID
+		
+		_x = cha.getX();
+		_y = cha.getY();
+		_z = cha.getZ();
+		
+		switch (rideClassId)
+		{
+			case 0: // dismount
+				_rideType = 0;
+				break;
+			case 12526: // Wind
+			case 12527: // Star
+			case 12528: // Twilight
+			case 16038: // red strider of wind
 			case 16039: // red strider of star
 			case 16040: // red strider of dusk
-                _rideType = 1;
-                break;
-            case 12621: // Wyvern
-                _rideType = 2;
-                break;	
-            case 16037: // Great Snow Wolf    
-            case 16041: // Fenrir Wolf	
-            case 16042: // White Fenrir Wolf	
-                _rideType = 3;
-                break;
-            case 13130: // Light Purple Maned Horse
-            	_rideType = 4;
-            	break;
-            default:
-                throw new IllegalArgumentException("Unsupported mount NpcId: "+rideClassId);
-        }
-    }
-
-    @Override
-    public void runImpl()
-    {
-
-    }
-
-    public int getMountType()
-    {
-        return _rideType;
-    }
-
-    @Override
+				_rideType = 1;
+				break;
+			case 12621: // Wyvern
+				_rideType = 2;
+				break;
+			case 16037: // Great Snow Wolf
+			case 16041: // Fenrir Wolf
+			case 16042: // White Fenrir Wolf
+				_rideType = 3;
+				break;
+			case 13130: // Light Purple Maned Horse
+			case 13146: // Tawny-Maned Lion
+			case 13147: // Steam Sledge
+				_rideType = 4;
+				break;
+			default:
+				throw new IllegalArgumentException("Unsupported mount NpcId: " + rideClassId);
+		}
+	}
+	
+	@Override
+	public void runImpl()
+	{
+		
+	}
+	
+	public int getMountType()
+	{
+		return _rideType;
+	}
+	
+	@Override
 	protected final void writeImpl()
-    {
-
-        writeC(0x8c);
-        writeD(_id);
-        writeD(_bRide);
-        writeD(_rideType);
-        writeD(_rideClassID);
-        writeD(_x);
-        writeD(_y);
-        writeD(_z);
-    }
-
-    /* (non-Javadoc)
-     * @see net.sf.l2j.gameserver.serverpackets.ServerBasePacket#getType()
-     */
-    @Override
+	{
+		writeC(0x8c);
+		writeD(_id);
+		writeD(_bRide);
+		writeD(_rideType);
+		writeD(_rideClassID);
+		writeD(_x);
+		writeD(_y);
+		writeD(_z);
+	}
+	
+	/* (non-Javadoc)
+	 * @see net.sf.l2j.gameserver.serverpackets.ServerBasePacket#getType()
+	 */
+	@Override
 	public String getType()
-    {
-        return _S__8c_Ride;
-    }
+	{
+		return _S__8c_Ride;
+	}
 }
