78b4fdd37471a667b3a195a5d8c9254c8a11de71
_DS_ <_DS_@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2009-07-31 06:52:24 +0000

    Synchronizing updateDatabase() function to prevent "duplicate" SQL errors
    git-svn-id: http://svn.l2jserver.com/trunk@3333 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer/java/net/sf/l2j/gameserver/model/L2ItemInstance.java
+++ L2_GameServer/java/net/sf/l2j/gameserver/model/L2ItemInstance.java
@@ -1282,7 +1282,7 @@ public final class L2ItemInstance extends L2Object
      * 
      * @param force if the update should necessarilly be done.
      */
-	public void updateDatabase(boolean force)
+	public synchronized void updateDatabase(boolean force)
 	{
 		if (isWear()) //avoid saving weared items
 		{
