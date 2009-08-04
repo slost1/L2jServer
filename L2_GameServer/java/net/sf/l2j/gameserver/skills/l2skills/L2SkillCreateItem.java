7e1a171928830730a497f516ba70ec0e4d481da3
_DS_ <_DS_@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2009-08-04 12:22:53 +0000

    Fixed problem with items created by skill (Kamael Bolts, for example) - they can't be placed into warehouse, for example. Using more proper way to add item into inventory.
    git-svn-id: http://svn.l2jserver.com/trunk@3351 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer/java/net/sf/l2j/gameserver/skills/l2skills/L2SkillCreateItem.java
+++ L2_GameServer/java/net/sf/l2j/gameserver/skills/l2skills/L2SkillCreateItem.java
@@ -14,14 +14,11 @@
  */
 package net.sf.l2j.gameserver.skills.l2skills;
 
-import net.sf.l2j.gameserver.idfactory.IdFactory;
-import net.sf.l2j.gameserver.model.L2ItemInstance;
 import net.sf.l2j.gameserver.model.L2Object;
 import net.sf.l2j.gameserver.model.L2Skill;
 import net.sf.l2j.gameserver.model.actor.L2Character;
 import net.sf.l2j.gameserver.model.actor.instance.L2PcInstance;
 import net.sf.l2j.gameserver.network.SystemMessageId;
-import net.sf.l2j.gameserver.network.serverpackets.ItemList;
 import net.sf.l2j.gameserver.network.serverpackets.SystemMessage;
 import net.sf.l2j.gameserver.templates.StatsSet;
 import net.sf.l2j.util.Rnd;
@@ -63,37 +60,7 @@ public class L2SkillCreateItem extends L2Skill
 
 			int count = _createItemCount + Rnd.nextInt(_randomCount);
 			int rndid = Rnd.nextInt(_createItemId.length);
-			giveItems((L2PcInstance) activeChar, _createItemId[rndid], count);
+			((L2PcInstance)activeChar).addItem("Skill", _createItemId[rndid], count, activeChar, true);
 		}
 	}
-
-	/**
-	 * @param activeChar
-	 * @param itemId
-	 * @param count
-	 */
-	public void giveItems(L2PcInstance activeChar, int itemId, int count)
-	{
-		L2ItemInstance item = new L2ItemInstance(IdFactory.getInstance().getNextId(), itemId);
-		if (item == null)
-			return;
-		item.setCount(count);
-		activeChar.getInventory().addItem("Skill", item, activeChar, activeChar);
-
-		if (count > 1)
-		{
-			SystemMessage smsg = new SystemMessage(SystemMessageId.EARNED_S2_S1_S);
-			smsg.addItemName(item);
-			smsg.addItemNumber(count);
-			activeChar.sendPacket(smsg);
-		}
-		else
-		{
-			SystemMessage smsg = new SystemMessage(SystemMessageId.EARNED_ITEM);
-			smsg.addItemName(item);
-			activeChar.sendPacket(smsg);
-		}
-		ItemList il = new ItemList(activeChar, false);
-		activeChar.sendPacket(il);
-    }
 }
