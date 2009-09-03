3fb849e2857eb6bc2615ec63ffce66620cf2e1c5
_DS_ <_DS_@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2009-09-03 07:18:31 +0000

    Fix decrementing start level :) Thanks ZumbuL for reporting.
    git-svn-id: http://svn.l2jserver.com/trunk@3448 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer/java/net/sf/l2j/gameserver/network/clientpackets/CharacterCreate.java
+++ L2_GameServer/java/net/sf/l2j/gameserver/network/clientpackets/CharacterCreate.java
@@ -199,7 +199,7 @@ public final class CharacterCreate extends L2GameClientPacket
 
 		if (Config.STARTING_LEVEL > 1)
 		{
-			newChar.getStat().addLevel(--Config.STARTING_LEVEL);
+			newChar.getStat().addLevel((byte)(Config.STARTING_LEVEL - 1));
 		}
 		if (Config.STARTING_SP > 0)
 		{
