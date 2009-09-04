ae8b7ef12699e7abfbdb4a78c135dde056a06edc
_DS_ <_DS_@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2009-09-04 19:10:48 +0000

    Fix for next action after skill, thanks XavierElf.
    git-svn-id: http://svn.l2jserver.com/trunk@3451 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer/java/net/sf/l2j/gameserver/model/actor/L2Character.java
+++ L2_GameServer/java/net/sf/l2j/gameserver/model/actor/L2Character.java
@@ -6259,9 +6259,9 @@ public abstract class L2Character extends L2Object
 		// Note: this might also work
 		// if (skill.isOffensive() && getAI().getNextIntention() == null
 		// && !(skill.getSkillType() == SkillType.UNLOCK) && !(skill.getSkillType() == SkillType.DELUXE_KEY_UNLOCK) && !(skill.getSkillType() == SkillType.MDAM))
-		if (getAI().getNextIntention() == null && skill.getSkillType() == L2SkillType.PDAM || skill.getSkillType() == L2SkillType.BLOW
+		if (getAI().getNextIntention() == null && (skill.getSkillType() == L2SkillType.PDAM || skill.getSkillType() == L2SkillType.BLOW
 				|| skill.getSkillType() == L2SkillType.DRAIN_SOUL || skill.getSkillType() == L2SkillType.SOW || skill.getSkillType() == L2SkillType.CHARGEDAM
-				|| skill.getSkillType() == L2SkillType.SPOIL)
+				|| skill.getSkillType() == L2SkillType.SPOIL))
 		{
 			if (getTarget() instanceof L2Character && getTarget() != this && target == getTarget())
 				getAI().setIntention(CtrlIntention.AI_INTENTION_ATTACK, getTarget());
