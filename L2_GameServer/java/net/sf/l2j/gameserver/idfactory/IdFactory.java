c568e8883813faae1044105d36235a1ea40532de
nBd <nBd@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2009-09-10 20:31:20 +0000

    Making startup for servers with big database much faster. Take note that tempobjectid table is unused now!
    git-svn-id: http://svn.l2jserver.com/trunk@3481 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer/java/net/sf/l2j/gameserver/idfactory/IdFactory.java
+++ L2_GameServer/java/net/sf/l2j/gameserver/idfactory/IdFactory.java
@@ -1,18 +1,18 @@
 /*
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  * 
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License along with
- * this program. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
  */
 package net.sf.l2j.gameserver.idfactory;
+
+import gnu.trove.TIntArrayList;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -25,7 +25,7 @@ import net.sf.l2j.L2DatabaseFactory;
 
 /**
  * This class ...
- *
+ * 
  * @version $Revision: 1.3.2.1.2.7 $ $Date: 2005/04/11 10:06:12 $
  */
 public abstract class IdFactory
@@ -33,53 +33,53 @@ public abstract class IdFactory
 	private static Logger _log = Logger.getLogger(IdFactory.class.getName());
 	
 	protected static final String[] ID_UPDATES = {
-			"UPDATE items                 SET owner_id = ?    WHERE owner_id = ?",
-			"UPDATE items                 SET object_id = ?   WHERE object_id = ?",
-			"UPDATE character_quests      SET charId = ?     WHERE charId = ?",
-			"UPDATE character_friends     SET charId = ?     WHERE charId = ?",
-			"UPDATE character_friends     SET friendId = ?   WHERE friendId = ?",
-			"UPDATE character_hennas      SET charId = ? WHERE charId = ?",
-			"UPDATE character_recipebook  SET charId = ? WHERE charId = ?",
-			"UPDATE character_shortcuts   SET charId = ? WHERE charId = ?",
-			"UPDATE character_shortcuts   SET shortcut_id = ? WHERE shortcut_id = ? AND type = 1", // items
-			"UPDATE character_macroses    SET charId = ? WHERE charId = ?", "UPDATE character_skills      SET charId = ? WHERE charId = ?",
-			"UPDATE character_skills_save SET charId = ? WHERE charId = ?", "UPDATE character_subclasses  SET charId = ? WHERE charId = ?",
-			"UPDATE characters            SET charId = ? WHERE charId = ?",
-			"UPDATE characters            SET clanid = ?      WHERE clanid = ?",
-			"UPDATE clan_data             SET clan_id = ?     WHERE clan_id = ?",
-			"UPDATE siege_clans           SET clan_id = ?     WHERE clan_id = ?",
-			"UPDATE clan_data             SET ally_id = ?     WHERE ally_id = ?",
-			"UPDATE clan_data             SET leader_id = ?   WHERE leader_id = ?",
-			"UPDATE pets                  SET item_obj_id = ? WHERE item_obj_id = ?",
-			"UPDATE character_hennas     SET charId = ? WHERE charId = ?",
-			"UPDATE itemsonground         SET object_id = ?   WHERE object_id = ?",
-			"UPDATE auction_bid          SET bidderId = ?      WHERE bidderId = ?",
-			"UPDATE auction_watch        SET charObjId = ?     WHERE charObjId = ?",
-			"UPDATE clanhall             SET ownerId = ?       WHERE ownerId = ?"
-	};
+	        "UPDATE items                 SET owner_id = ?    WHERE owner_id = ?",
+	        "UPDATE items                 SET object_id = ?   WHERE object_id = ?",
+	        "UPDATE character_quests      SET charId = ?     WHERE charId = ?",
+	        "UPDATE character_friends     SET charId = ?     WHERE charId = ?",
+	        "UPDATE character_friends     SET friendId = ?   WHERE friendId = ?",
+	        "UPDATE character_hennas      SET charId = ? WHERE charId = ?",
+	        "UPDATE character_recipebook  SET charId = ? WHERE charId = ?",
+	        "UPDATE character_shortcuts   SET charId = ? WHERE charId = ?",
+	        "UPDATE character_shortcuts   SET shortcut_id = ? WHERE shortcut_id = ? AND type = 1", // items
+	        "UPDATE character_macroses    SET charId = ? WHERE charId = ?",
+	        "UPDATE character_skills      SET charId = ? WHERE charId = ?",
+	        "UPDATE character_skills_save SET charId = ? WHERE charId = ?",
+	        "UPDATE character_subclasses  SET charId = ? WHERE charId = ?",
+	        "UPDATE characters            SET charId = ? WHERE charId = ?",
+	        "UPDATE characters            SET clanid = ?      WHERE clanid = ?",
+	        "UPDATE clan_data             SET clan_id = ?     WHERE clan_id = ?",
+	        "UPDATE siege_clans           SET clan_id = ?     WHERE clan_id = ?",
+	        "UPDATE clan_data             SET ally_id = ?     WHERE ally_id = ?",
+	        "UPDATE clan_data             SET leader_id = ?   WHERE leader_id = ?",
+	        "UPDATE pets                  SET item_obj_id = ? WHERE item_obj_id = ?",
+	        "UPDATE character_hennas     SET charId = ? WHERE charId = ?",
+	        "UPDATE itemsonground         SET object_id = ?   WHERE object_id = ?",
+	        "UPDATE auction_bid          SET bidderId = ?      WHERE bidderId = ?",
+	        "UPDATE auction_watch        SET charObjId = ?     WHERE charObjId = ?",
+	        "UPDATE clanhall             SET ownerId = ?       WHERE ownerId = ?" };
 	
 	protected static final String[] ID_CHECKS = {
-			"SELECT owner_id    FROM items                 WHERE object_id >= ?   AND object_id < ?",
-			"SELECT object_id   FROM items                 WHERE object_id >= ?   AND object_id < ?",
-			"SELECT charId     FROM character_quests      WHERE charId >= ?     AND charId < ?",
-			"SELECT charId     FROM character_friends     WHERE charId >= ?     AND charId < ?",
-			"SELECT charId     FROM character_friends     WHERE friendId >= ?   AND friendId < ?",
-			"SELECT charId     FROM character_hennas      WHERE charId >= ? AND charId < ?",
-			"SELECT charId     FROM character_recipebook  WHERE charId >= ?     AND charId < ?",
-			"SELECT charId     FROM character_shortcuts   WHERE charId >= ? AND charId < ?",
-			"SELECT charId     FROM character_macroses    WHERE charId >= ? AND charId < ?",
-			"SELECT charId     FROM character_skills      WHERE charId >= ? AND charId < ?",
-			"SELECT charId     FROM character_skills_save WHERE charId >= ? AND charId < ?",
-			"SELECT charId     FROM character_subclasses  WHERE charId >= ? AND charId < ?",
-			"SELECT charId      FROM characters            WHERE charId >= ?      AND charId < ?",
-			"SELECT clanid      FROM characters            WHERE clanid >= ?      AND clanid < ?",
-			"SELECT clan_id     FROM clan_data             WHERE clan_id >= ?     AND clan_id < ?",
-			"SELECT clan_id     FROM siege_clans           WHERE clan_id >= ?     AND clan_id < ?",
-			"SELECT ally_id     FROM clan_data             WHERE ally_id >= ?     AND ally_id < ?",
-			"SELECT leader_id   FROM clan_data             WHERE leader_id >= ?   AND leader_id < ?",
-			"SELECT item_obj_id FROM pets                  WHERE item_obj_id >= ? AND item_obj_id < ?",
-			"SELECT object_id   FROM itemsonground        WHERE object_id >= ?   AND object_id < ?"
-	};
+	        "SELECT owner_id    FROM items                 WHERE object_id >= ?   AND object_id < ?",
+	        "SELECT object_id   FROM items                 WHERE object_id >= ?   AND object_id < ?",
+	        "SELECT charId     FROM character_quests      WHERE charId >= ?     AND charId < ?",
+	        "SELECT charId     FROM character_friends     WHERE charId >= ?     AND charId < ?",
+	        "SELECT charId     FROM character_friends     WHERE friendId >= ?   AND friendId < ?",
+	        "SELECT charId     FROM character_hennas      WHERE charId >= ? AND charId < ?",
+	        "SELECT charId     FROM character_recipebook  WHERE charId >= ?     AND charId < ?",
+	        "SELECT charId     FROM character_shortcuts   WHERE charId >= ? AND charId < ?",
+	        "SELECT charId     FROM character_macroses    WHERE charId >= ? AND charId < ?",
+	        "SELECT charId     FROM character_skills      WHERE charId >= ? AND charId < ?",
+	        "SELECT charId     FROM character_skills_save WHERE charId >= ? AND charId < ?",
+	        "SELECT charId     FROM character_subclasses  WHERE charId >= ? AND charId < ?",
+	        "SELECT charId      FROM characters            WHERE charId >= ?      AND charId < ?",
+	        "SELECT clanid      FROM characters            WHERE clanid >= ?      AND clanid < ?",
+	        "SELECT clan_id     FROM clan_data             WHERE clan_id >= ?     AND clan_id < ?",
+	        "SELECT clan_id     FROM siege_clans           WHERE clan_id >= ?     AND clan_id < ?",
+	        "SELECT ally_id     FROM clan_data             WHERE ally_id >= ?     AND ally_id < ?",
+	        "SELECT leader_id   FROM clan_data             WHERE leader_id >= ?   AND leader_id < ?",
+	        "SELECT item_obj_id FROM pets                  WHERE item_obj_id >= ? AND item_obj_id < ?",
+	        "SELECT object_id   FROM itemsonground        WHERE object_id >= ?   AND object_id < ?" };
 	
 	protected boolean _initialized;
 	
@@ -156,18 +156,28 @@ public abstract class IdFactory
 			con = L2DatabaseFactory.getInstance().getConnection();
 			Statement stmt = con.createStatement();
 			// Misc/Account Related
-			// Please read the descriptions above each before uncommenting them. If you are still unsure of what exactly it does, leave it commented out. This is for those who know what they are doing. :)
-
-			// Deletes only accounts that HAVE been logged into and have no characters associated with the account.
-			// cleanCount += stmt.executeUpdate("DELETE FROM accounts WHERE accounts.lastactive > 0 AND accounts.login NOT IN (SELECT account_name FROM characters);");
-
-			// Deletes any accounts that don't have characters. Whether or not the player has ever logged into the account.
-			// cleanCount += stmt.executeUpdate("DELETE FROM accounts WHERE accounts.login NOT IN (SELECT account_name FROM characters);");
-
-			// Deletes banned accounts that have not been logged into for xx amount of days (specified at the end of the script, default is set to 90 days). This prevents accounts from being deleted that were accidentally or temporarily banned.
-			//cleanCount += stmt.executeUpdate("DELETE FROM accounts WHERE accounts.accessLevel < 0 AND DATEDIFF(CURRENT_DATE( ) , FROM_UNIXTIME(`lastactive`/1000)) > 90;");
-			//cleanCount += stmt.executeUpdate("DELETE FROM characters WHERE characters.account_name NOT IN (SELECT login FROM accounts);");
-
+			// Please read the descriptions above each before uncommenting them. If you are still
+			// unsure of what exactly it does, leave it commented out. This is for those who know
+			// what they are doing. :)
+			
+			// Deletes only accounts that HAVE been logged into and have no characters associated
+			// with the account.
+			// cleanCount +=
+			// stmt.executeUpdate("DELETE FROM accounts WHERE accounts.lastactive > 0 AND accounts.login NOT IN (SELECT account_name FROM characters);");
+			
+			// Deletes any accounts that don't have characters. Whether or not the player has ever
+			// logged into the account.
+			// cleanCount +=
+			// stmt.executeUpdate("DELETE FROM accounts WHERE accounts.login NOT IN (SELECT account_name FROM characters);");
+			
+			// Deletes banned accounts that have not been logged into for xx amount of days
+			// (specified at the end of the script, default is set to 90 days). This prevents
+			// accounts from being deleted that were accidentally or temporarily banned.
+			// cleanCount +=
+			// stmt.executeUpdate("DELETE FROM accounts WHERE accounts.accessLevel < 0 AND DATEDIFF(CURRENT_DATE( ) , FROM_UNIXTIME(`lastactive`/1000)) > 90;");
+			// cleanCount +=
+			// stmt.executeUpdate("DELETE FROM characters WHERE characters.account_name NOT IN (SELECT login FROM accounts);");
+			
 			// If the character does not exist...
 			cleanCount += stmt.executeUpdate("DELETE FROM character_friends WHERE character_friends.charId NOT IN (SELECT charId FROM characters);");
 			cleanCount += stmt.executeUpdate("DELETE FROM character_friends WHERE character_friends.friendId NOT IN (SELECT charId FROM characters);");
@@ -192,7 +202,7 @@ public abstract class IdFactory
 			cleanCount += stmt.executeUpdate("DELETE FROM character_recommends WHERE character_recommends.charId NOT IN (SELECT charId FROM characters);");
 			cleanCount += stmt.executeUpdate("DELETE FROM character_recommends WHERE character_recommends.target_id NOT IN (SELECT charId FROM characters);");
 			cleanCount += stmt.executeUpdate("DELETE FROM clan_data WHERE clan_data.leader_id NOT IN (SELECT charId FROM characters);");
-
+			
 			// If the clan does not exist...
 			cleanCount += stmt.executeUpdate("DELETE FROM clan_privs WHERE clan_privs.clan_id NOT IN (SELECT clan_id FROM clan_data);");
 			cleanCount += stmt.executeUpdate("DELETE FROM clan_skills WHERE clan_skills.clan_id NOT IN (SELECT clan_id FROM clan_data);");
@@ -203,15 +213,19 @@ public abstract class IdFactory
 			cleanCount += stmt.executeUpdate("DELETE FROM siege_clans WHERE siege_clans.clan_id NOT IN (SELECT clan_id FROM clan_data);");
 			cleanCount += stmt.executeUpdate("DELETE FROM clan_notices WHERE clan_notices.clan_id NOT IN (SELECT clan_id FROM clan_data);");
 			cleanCount += stmt.executeUpdate("DELETE FROM auction_bid WHERE auction_bid.bidderId NOT IN (SELECT clan_id FROM clan_data);");
-			//Untested, leaving commented out until confirmation that it's safe/works properly. Was initially removed because of a bug. Search for idfactory.java changes in the trac for further info.
-			//cleanCount += stmt.executeUpdate("DELETE FROM auction WHERE auction.id IN (SELECT id FROM clanhall WHERE ownerId <> 0) AND auction.sellerId=0;");
-			//cleanCount += stmt.executeUpdate("DELETE FROM auction_bid WHERE auctionId NOT IN (SELECT id FROM auction)");
-
-			//Forum Related
+			// Untested, leaving commented out until confirmation that it's safe/works properly. Was
+			// initially removed because of a bug. Search for idfactory.java changes in the trac for
+			// further info.
+			// cleanCount +=
+			// stmt.executeUpdate("DELETE FROM auction WHERE auction.id IN (SELECT id FROM clanhall WHERE ownerId <> 0) AND auction.sellerId=0;");
+			// cleanCount +=
+			// stmt.executeUpdate("DELETE FROM auction_bid WHERE auctionId NOT IN (SELECT id FROM auction)");
+			
+			// Forum Related
 			cleanCount += stmt.executeUpdate("DELETE FROM forums WHERE forums.forum_owner_id NOT IN (SELECT clan_id FROM clan_data) AND forums.forum_parent=2;");
 			cleanCount += stmt.executeUpdate("DELETE FROM posts WHERE posts.post_forum_id NOT IN (SELECT forum_id FROM forums);");
 			cleanCount += stmt.executeUpdate("DELETE FROM topic WHERE topic.topic_forum_id NOT IN (SELECT forum_id FROM forums);");
-
+			
 			// Update needed items after cleaning has taken place.
 			stmt.executeUpdate("UPDATE clan_data SET auction_bid_at = 0 WHERE auction_bid_at NOT IN (SELECT auctionId FROM auction_bid);");
 			stmt.executeUpdate("UPDATE clan_subpledges SET leader_id=0 WHERE clan_subpledges.leader_id NOT IN (SELECT charId FROM characters) AND leader_id > 0;");
@@ -242,46 +256,73 @@ public abstract class IdFactory
 	 * @return
 	 * @throws SQLException
 	 */
-	protected int[] extractUsedObjectIDTable() throws SQLException
+	protected final int[] extractUsedObjectIDTable() throws Exception
 	{
 		Connection con = null;
+		
 		try
 		{
 			con = L2DatabaseFactory.getInstance().getConnection();
 			
-			//create a temporary table
-			Statement statement = con.createStatement();
+			Statement statement = null;
+			ResultSet rset = null;
 			
-			statement.executeUpdate("DROP TABLE IF EXISTS temporaryObjectTable");
-			statement.executeUpdate("DELETE FROM itemsonground WHERE object_id IN (SELECT object_id FROM items)");
-			statement.executeUpdate("CREATE TABLE temporaryObjectTable" + " (object_id int NOT NULL PRIMARY KEY)");
-			
-			statement.executeUpdate("INSERT INTO temporaryObjectTable (object_id)" + " SELECT charId FROM characters");
-			statement.executeUpdate("INSERT INTO temporaryObjectTable (object_id)" + " SELECT object_id FROM items");
-			statement.executeUpdate("INSERT INTO temporaryObjectTable (object_id)" + " SELECT clan_id FROM clan_data");
-			//statement.executeUpdate("INSERT INTO temporaryObjectTable (object_id)" + " SELECT crest_id FROM clan_data WHERE crest_id > 0");
-			statement.executeUpdate("INSERT INTO temporaryObjectTable (object_id)" + " SELECT object_id FROM itemsonground");
-			
-			ResultSet result = statement.executeQuery("SELECT COUNT(object_id) FROM temporaryObjectTable");
-			
-			result.next();
-			int size = result.getInt(1);
-			result.close();
-			int[] tmp_obj_ids = new int[size];
-			//_log.info("tmp table size: " + tmp_obj_ids.length);
-			
-			result = statement.executeQuery("SELECT object_id FROM temporaryObjectTable ORDER BY object_id");
-			
-			int idx = 0;
-			while (result.next())
+			try
 			{
-				tmp_obj_ids[idx++] = result.getInt(1);
+				statement = con.createStatement();				
+				final TIntArrayList temp = new TIntArrayList();
+				
+				rset = statement.executeQuery("SELECT COUNT(*) FROM characters");
+				rset.next();
+				temp.ensureCapacity(rset.getInt(1));
+				rset = statement.executeQuery("SELECT charId FROM characters");
+				while (rset.next())
+				{
+					temp.add(rset.getInt(1));
+				}
+				
+				rset = statement.executeQuery("SELECT COUNT(*) FROM items");
+				rset.next();
+				temp.ensureCapacity(temp.size() + rset.getInt(1));
+				rset = statement.executeQuery("SELECT object_id FROM items");
+				while (rset.next())
+				{
+					temp.add(rset.getInt(1));
+				}
+				
+				rset = statement.executeQuery("SELECT COUNT(*) FROM clan_data");
+				rset.next();
+				temp.ensureCapacity(temp.size() + rset.getInt(1));
+				rset = statement.executeQuery("SELECT clan_id FROM clan_data");
+				while (rset.next())
+				{
+					temp.add(rset.getInt(1));
+				}
+				
+				rset = statement.executeQuery("SELECT COUNT(*) FROM itemsonground");
+				rset.next();
+				temp.ensureCapacity(temp.size() + rset.getInt(1));
+				rset = statement.executeQuery("SELECT object_id FROM itemsonground");
+				while (rset.next())
+				{
+					temp.add(rset.getInt(1));
+				}
+				
+				temp.sort();
+				
+				return temp.toNativeArray();
 			}
-			
-			result.close();
-			statement.close();
-			
-			return tmp_obj_ids;
+			finally
+			{
+				try
+				{
+					statement.close();
+				}
+				catch (Exception e)
+				{
+					
+				}
+			}
 		}
 		finally
 		{
@@ -291,6 +332,7 @@ public abstract class IdFactory
 			}
 			catch (Exception e)
 			{
+				
 			}
 		}
 	}
@@ -309,7 +351,9 @@ public abstract class IdFactory
 	
 	/**
 	 * return a used Object ID back to the pool
-	 * @param object ID
+	 * 
+	 * @param object
+	 *            ID
 	 */
 	public abstract void releaseId(int id);
 	
