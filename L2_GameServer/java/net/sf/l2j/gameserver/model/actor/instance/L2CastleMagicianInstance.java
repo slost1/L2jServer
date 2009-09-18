29c2c4b91655308c2500c6a67061e8c2bf8c5e73
Charus <Charus@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2009-09-18 05:02:18 +0000

    Castle Magician update. Need dp support.
    git-svn-id: http://svn.l2jserver.com/trunk@3525 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer/java/net/sf/l2j/gameserver/model/actor/instance/L2CastleMagicianInstance.java
+++ L2_GameServer/java/net/sf/l2j/gameserver/model/actor/instance/L2CastleMagicianInstance.java
@@ -16,24 +16,35 @@ package net.sf.l2j.gameserver.model.actor.instance;
 
 import net.sf.l2j.Config;
 import net.sf.l2j.gameserver.SevenSigns;
+import net.sf.l2j.gameserver.network.SystemMessageId;
 import net.sf.l2j.gameserver.network.serverpackets.ActionFailed;
 import net.sf.l2j.gameserver.network.serverpackets.NpcHtmlMessage;
+import net.sf.l2j.gameserver.network.serverpackets.SystemMessage;
 import net.sf.l2j.gameserver.templates.chars.L2NpcTemplate;
 import net.sf.l2j.gameserver.templates.skills.L2EffectType;
 import net.sf.l2j.gameserver.model.actor.L2Character;
 import net.sf.l2j.gameserver.model.entity.TvTEvent;
 import net.sf.l2j.gameserver.instancemanager.InstanceManager;
-
+import net.sf.l2j.util.Rnd;
 
 /**
  * @author Kerberos | ZaKaX
  */
-
 public class L2CastleMagicianInstance extends L2NpcInstance
 {
 	protected static final int COND_ALL_FALSE = 0;
 	protected static final int COND_BUSY_BECAUSE_OF_SIEGE = 1;
 	protected static final int COND_OWNER = 2;
+
+	private final int[] TalismanIds =
+	{
+		9914,9915,9917,9918,9919,9920,9921,9922,9923,9924,
+		9926,9927,9928,9930,9931,9932,9933,9934,9935,9936,
+		9937,9938,9939,9940,9941,9942,9943,9944,9945,9946,
+		9947,9948,9949,9950,9951,9952,9953,9954,9955,9956,
+		9957,9958,9959,9960,9961,9962,9963,9964,9965,9966,
+		10141,10142,10158
+	};
 
 	/**
 	* @param template
@@ -66,7 +77,6 @@ public class L2CastleMagicianInstance extends L2NpcInstance
 		NpcHtmlMessage html = new NpcHtmlMessage(getObjectId());
 		html.setFile(filename);
 		html.replace("%objectId%", String.valueOf(getObjectId()));
-		html.replace("%npcname%", String.valueOf(getName()+" "+getTitle()));
 		player.sendPacket(html);
 	}
 
@@ -84,6 +94,32 @@ public class L2CastleMagicianInstance extends L2NpcInstance
 			catch (NumberFormatException nfe){}
 			showChatWindow(player, val);
 		}
+		else if (command.equals("Talisman"))
+		{
+			String filename = "data/html/castlemagician/magician-Talisman.htm";
+			showChatWindow(player, filename);
+		}
+		else if (command.startsWith("ExchangeKE"))
+		{
+			String filename = null;
+			int item = TalismanIds[Rnd.get(TalismanIds.length)];
+
+			if (player.destroyItemByItemId("ExchangeKE", 9912, 10, this, false))
+			{
+				SystemMessage msg = new SystemMessage(SystemMessageId.S2_S1_DISAPPEARED);
+				msg.addItemName(9912);
+				msg.addNumber(10);
+				player.sendPacket(msg);
+
+				player.addItem("ExchangeKE", item, 1, player, true);
+
+				filename = "data/html/castlemagician/magician-KE-Exchange.htm";
+			}
+			else
+				filename = "data/html/castlemagician/magician-no-KE.htm";
+
+			showChatWindow(player, filename);
+		}
 		else if (command.equals("gotoleader"))
 		{
 			if (player.getClan() != null)
@@ -97,11 +133,11 @@ public class L2CastleMagicianInstance extends L2NpcInstance
 					if (!validateGateCondition(clanLeader, player))
 						return;
 
-					player.sendMessage("You will be teleported throught the Clan Gate now.");
 					player.teleToLocation(clanLeader.getX(), clanLeader.getY(), clanLeader.getZ(), false);
 					return;
 				}
-				player.sendMessage("I cannot find the lord's signal right now, so it is impossible to cast the spell.");
+				String filename = "data/html/castlemagician/magician-nogate.htm";
+				showChatWindow(player, filename);
 			}
 		}
 		else
