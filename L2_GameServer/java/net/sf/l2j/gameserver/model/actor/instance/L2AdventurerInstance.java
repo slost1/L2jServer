ea49b5766d1df41d0883c8206449b2379beab268
Charus <Charus@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2009-09-18 05:06:19 +0000

    Adventurer instance clean up
    git-svn-id: http://svn.l2jserver.com/trunk@3526 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer/java/net/sf/l2j/gameserver/model/actor/instance/L2AdventurerInstance.java
+++ L2_GameServer/java/net/sf/l2j/gameserver/model/actor/instance/L2AdventurerInstance.java
@@ -14,10 +14,7 @@
  */
 package net.sf.l2j.gameserver.model.actor.instance;
 
-import net.sf.l2j.gameserver.instancemanager.RaidBossSpawnManager;
-import net.sf.l2j.gameserver.model.L2Spawn;
 import net.sf.l2j.gameserver.network.serverpackets.ExShowQuestInfo;
-import net.sf.l2j.gameserver.network.serverpackets.RadarControl;
 import net.sf.l2j.gameserver.templates.chars.L2NpcTemplate;
 
 /**
@@ -28,68 +25,30 @@ import net.sf.l2j.gameserver.templates.chars.L2NpcTemplate;
  */
 public class L2AdventurerInstance extends L2NpcInstance
 {
-    //private static Logger _log = Logger.getLogger(L2AdventurerInstance.class.getName());
+	public L2AdventurerInstance(int objectId, L2NpcTemplate template)
+	{
+		super(objectId, template);
+	}
 
-    public L2AdventurerInstance(int objectId, L2NpcTemplate template)
-    {
-        super(objectId, template);
-    }
-
-    @Override
+	@Override
 	public void onBypassFeedback(L2PcInstance player, String command)
-    {
-        if (command.startsWith("npcfind_byid"))
-        {
-            try
-            {
-                int bossId = Integer.parseInt(command.substring(12).trim());
-                switch (RaidBossSpawnManager.getInstance().getRaidBossStatusId(bossId))
-                {
-                    case ALIVE:
-                    case DEAD:
-                        L2Spawn spawn = RaidBossSpawnManager.getInstance().getSpawns().get(bossId);
-                        player.sendPacket(new RadarControl(2, 2, spawn.getLocx(), spawn.getLocy(), spawn.getLocz()));
-                        player.sendPacket(new RadarControl(0, 1, spawn.getLocx(), spawn.getLocy(), spawn.getLocz()));
-                        break;
-                    case UNDEFINED:
-                        player.sendMessage("This Boss isn't in game - notify L2J Datapack Dev Team");
-                        break;
-                }
-            }
-            catch (NumberFormatException e)
-            {
-                _log.warning("Invalid Bypass to Server command parameter.");
-            }
-        }
-        else if (command.startsWith("raidInfo"))
-        {
-            int bossLevel = Integer.parseInt(command.substring(9).trim());
-            String filename = "data/html/adventurer_guildsman/raid_info/info.htm";
-            if (bossLevel != 0) { filename = "data/html/adventurer_guildsman/raid_info/level" + bossLevel + ".htm"; }
-            showChatWindow(player, bossLevel, filename);
-        }
-        else if (command.equalsIgnoreCase("questlist"))
-        {
-            player.sendPacket(new ExShowQuestInfo());
-        }
-        else
-        {
-            super.onBypassFeedback(player, command);
-        }
-    }
+	{
+		if (command.equalsIgnoreCase("questlist"))
+			player.sendPacket(new ExShowQuestInfo());
+		else
+			super.onBypassFeedback(player, command);
+	}
 
-    @Override
+	@Override
 	public String getHtmlPath(int npcId, int val)
-    {
-        String pom = "";
+	{
+		String pom = "";
 
-        if (val == 0) pom = "" + npcId;
-        else pom = npcId + "-" + val;
+		if (val == 0)
+			pom = "" + npcId;
+		else
+			pom = npcId + "-" + val;
 
-        return "data/html/adventurer_guildsman/" + pom + ".htm";
-    }
-    private void showChatWindow(L2PcInstance player, int bossLevel, String filename)
-    {
-        showChatWindow(player, filename);
-    }
+		return "data/html/adventurer_guildsman/" + pom + ".htm";
+	}
 }
