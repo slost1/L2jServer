001ed561d373aef8de0adbd322c4547985634527
_DS_ <_DS_@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2009-11-02 17:53:39 +0000

    Forget about trade.
    git-svn-id: http://svn.l2jserver.com/trunk@3689 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer/java/net/sf/l2j/gameserver/network/clientpackets/TradeRequest.java
+++ L2_GameServer/java/net/sf/l2j/gameserver/network/clientpackets/TradeRequest.java
@@ -70,6 +70,11 @@ public final class TradeRequest extends L2GameClientPacket
 
         L2PcInstance partner = (L2PcInstance)target;
 
+        // cant trade with players from other instance except from multiverse
+        if (partner.getInstanceId() != player.getInstanceId()
+        		&& player.getInstanceId() != -1)
+        	return;
+
         if (partner.isInOlympiadMode() || player.isInOlympiadMode())
         {
             player.sendMessage("You or your target cant request trade in Olympiad mode");
