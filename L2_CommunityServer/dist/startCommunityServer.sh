664c656325cfabc7a8b771b8e30db4267e0e6367
JIV <JIV@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2009-12-24 11:23:51 +0000

    #4288
    git-svn-id: http://svn.l2jserver.com/trunk@3701 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_CommunityServer/dist/startCommunityServer.sh
+++ L2_CommunityServer/dist/startCommunityServer.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xms128m -Xmx128m -cp ./../libs/*:l2jcommunity.jar com.l2jserver.communityserver > log/stdout.log 2>&1
+java -Xms128m -Xmx128m -cp ./../libs/*:l2jcommunity.jar com.l2jserver.communityserver.L2CommunityServer > log/stdout.log 2>&1
