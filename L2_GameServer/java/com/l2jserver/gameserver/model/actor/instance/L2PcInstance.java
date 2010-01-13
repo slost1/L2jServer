2365e2ca4b09563d9f6d9550cd6498e00a191fb9
JIV <JIV@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2010-01-13 23:14:03 +0000

    missed file.
    git-svn-id: http://svn.l2jserver.com/trunk@3808 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer/java/com/l2jserver/gameserver/model/actor/instance/L2PcInstance.java
+++ L2_GameServer/java/com/l2jserver/gameserver/model/actor/instance/L2PcInstance.java
@@ -1915,7 +1915,14 @@ public final class L2PcInstance extends L2Playable
         if (Config.ALLOW_WATER)
             checkWaterState();
 
-        if (isInsideZone(ZONE_SIEGE))
+        if (isInsideZone(ZONE_DANGERAREA))
+        {
+        	if (_lastCompassZone == ExSetCompassZoneCode.ALTEREDZONE) return;
+        	_lastCompassZone = ExSetCompassZoneCode.ALTEREDZONE;
+        	ExSetCompassZoneCode cz = new ExSetCompassZoneCode(ExSetCompassZoneCode.ALTEREDZONE);
+        	sendPacket(cz);
+        }
+        else if (isInsideZone(ZONE_SIEGE))
         {
         	if (_lastCompassZone == ExSetCompassZoneCode.SIEGEWARZONE2) return;
         	_lastCompassZone = ExSetCompassZoneCode.SIEGEWARZONE2;
