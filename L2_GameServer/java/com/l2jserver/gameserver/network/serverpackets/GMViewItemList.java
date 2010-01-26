277d8e928efff249c98780d495fdc1acdb619052
JIV <JIV@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2010-01-26 15:18:33 +0000

    GmViewItemList support for pet inventory.
    git-svn-id: http://svn.l2jserver.com/trunk@3865 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer/java/com/l2jserver/gameserver/network/serverpackets/GMViewItemList.java
+++ L2_GameServer/java/com/l2jserver/gameserver/network/serverpackets/GMViewItemList.java
@@ -16,6 +16,7 @@ package com.l2jserver.gameserver.network.serverpackets;
 
 import com.l2jserver.gameserver.model.L2ItemInstance;
 import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
+import com.l2jserver.gameserver.model.actor.instance.L2PetInstance;
 
 /**
  *
@@ -26,14 +27,21 @@ public class GMViewItemList extends L2GameServerPacket
 	//private static Logger _log = Logger.getLogger(GMViewItemList.class.getName());
 	private static final String _S__AD_GMVIEWITEMLIST = "[S] 9a GMViewItemList";
 	private L2ItemInstance[] _items;
-	private L2PcInstance _cha;
+	private int _limit;
 	private String _playerName;
 	
 	public GMViewItemList(L2PcInstance cha)
 	{
 		_items = cha.getInventory().getItems();
 		_playerName = cha.getName();
-		_cha = cha;
+		_limit = cha.getInventoryLimit();
+	}
+	
+	public GMViewItemList(L2PetInstance cha)
+	{
+		_items = cha.getInventory().getItems();
+		_playerName = cha.getName();
+		_limit = cha.getInventoryLimit();
 	}
 	
 	@Override
@@ -41,7 +49,7 @@ public class GMViewItemList extends L2GameServerPacket
 	{
 		writeC(0x9a);
 		writeS(_playerName);
-		writeD(_cha.getInventoryLimit()); // inventory limit
+		writeD(_limit); // inventory limit
 		writeH(0x01); // show window ??
 		writeH(_items.length);
 		
