119d24ad04cf81382fdeb474f3492ae27c5d51bb
JIV <JIV@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2010-06-25 07:23:30 +0000

    classpath
    git-svn-id: http://svn.l2jserver.com/trunk@4301 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer/dist/L2JConfig.sh
+++ L2_GameServer/dist/L2JConfig.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Djava.util.logging.config.file=console.cfg -cp ./../libs/*:l2jserver.jar com.l2jserver.configurator.ConfigUserInterface
+java -Djava.util.logging.config.file=console.cfg -cp ./../libs/*:l2jserver.jar:l2jlogin.jar com.l2jserver.configurator.ConfigUserInterface
