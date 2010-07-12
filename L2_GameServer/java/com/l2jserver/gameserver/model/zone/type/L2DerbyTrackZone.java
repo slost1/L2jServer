a6023dac29c6fb3af42539d5f8451962aa20db61
JIV <JIV@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2010-07-12 22:12:23 +0000

    Fix for possible attack on summon in MDT.
    git-svn-id: http://svn.l2jserver.com/trunk@4368 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2_GameServer/java/com/l2jserver/gameserver/model/zone/type/L2DerbyTrackZone.java
+++ L2_GameServer/java/com/l2jserver/gameserver/model/zone/type/L2DerbyTrackZone.java
@@ -15,7 +15,7 @@
 package com.l2jserver.gameserver.model.zone.type;
 
 import com.l2jserver.gameserver.model.actor.L2Character;
-import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
+import com.l2jserver.gameserver.model.actor.L2Playable;
 import com.l2jserver.gameserver.model.zone.L2ZoneType;
 
 /**
@@ -33,7 +33,7 @@ public class L2DerbyTrackZone extends L2ZoneType
 	@Override
 	protected void onEnter(L2Character character)
 	{
-		if (character instanceof L2PcInstance)
+		if (character instanceof L2Playable)
 		{
 			character.setInsideZone(L2Character.ZONE_MONSTERTRACK, true);
 			character.setInsideZone(L2Character.ZONE_PEACE, true);
@@ -44,7 +44,7 @@ public class L2DerbyTrackZone extends L2ZoneType
 	@Override
 	protected void onExit(L2Character character)
 	{
-		if (character instanceof L2PcInstance)
+		if (character instanceof L2Playable)
 		{
 			character.setInsideZone(L2Character.ZONE_MONSTERTRACK, false);
 			character.setInsideZone(L2Character.ZONE_PEACE, false);
