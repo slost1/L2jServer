c52d05c7dced36fbc5c4cd83cefd7ffd8bd136c3
UnAfraid <UnAfraid@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2011-05-16 21:18:39 +0000

    BETA: Allowing not tradeable items for freight (Only Freightable ofc :P)
    git-svn-id: http://svn.l2jserver.com/branches/unstable@4625 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2J_Server_BETA/java/com/l2jserver/gameserver/network/clientpackets/RequestPackageSendableItemList.java
+++ L2J_Server_BETA/java/com/l2jserver/gameserver/network/clientpackets/RequestPackageSendableItemList.java
@@ -37,7 +37,7 @@ public class RequestPackageSendableItemList extends L2GameClientPacket
 	@Override
 	public void runImpl()
 	{
-		L2ItemInstance[] items = getClient().getActiveChar().getInventory().getAvailableItems(true, false, true);
+		L2ItemInstance[] items = getClient().getActiveChar().getInventory().getAvailableItems(true, true, true);
 		sendPacket(new PackageSendableList(items, _objectID));
 	}
 	
