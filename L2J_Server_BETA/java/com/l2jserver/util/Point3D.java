a541e266111e1bdb9cf1d335108e430dc6cd0f8f
Nik <Nik@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2011-06-14 12:25:53 +0000

    BETA: Removed synchronizations from Point3D. They are useless since we use volatile variables (thanks Szponiasty/Deedlit)
    git-svn-id: http://svn.l2jserver.com/branches/unstable@4676 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2J_Server_BETA/java/com/l2jserver/util/Point3D.java
+++ L2J_Server_BETA/java/com/l2jserver/util/Point3D.java
@@ -38,6 +38,7 @@ public class Point3D implements Serializable
 	 */
 	private static final long serialVersionUID = 4638345252031872576L;
 	
+	/* since using volatile vars, removing all synchronizations */
 	private volatile int _x, _y, _z;
 	
 	public Point3D(int pX, int pY, int pZ)
@@ -59,22 +60,16 @@ public class Point3D implements Serializable
 	 */
 	public Point3D(Point3D worldPosition)
 	{
-		synchronized (worldPosition)
-		{
-			_x = worldPosition._x;
-			_y = worldPosition._y;
-			_z = worldPosition._z;
-		}
+		_x = worldPosition._x;
+		_y = worldPosition._y;
+		_z = worldPosition._z;
 	}
 	
-	public synchronized void setTo(Point3D point)
+	public void setTo(Point3D point)
 	{
-		synchronized (point)
-		{
-			_x = point._x;
-			_y = point._y;
-			_z = point._z;
-		}
+		_x = point._x;
+		_y = point._y;
+		_z = point._z;
 	}
 	
 	@Override
@@ -90,48 +85,35 @@ public class Point3D implements Serializable
 	}
 	
 	@Override
-	public synchronized boolean equals(Object o)
+	public boolean equals(Object o)
 	{
 		if (o instanceof Point3D)
 		{
 			Point3D point3D = (Point3D) o;
-			boolean ret;
-			synchronized (point3D)
-			{
-				ret = point3D._x == _x && point3D._y == _y && point3D._z == _z;
-			}
+			boolean ret = point3D._x == _x && point3D._y == _y && point3D._z == _z;
 			return ret;
 		}
 		return false;
 	}
 	
-	public synchronized boolean equals(int pX, int pY, int pZ)
+	public boolean equals(int pX, int pY, int pZ)
 	{
 		return _x == pX && _y == pY && _z == pZ;
 	}
 	
-	public synchronized long distanceSquaredTo(Point3D point)
+	public long distanceSquaredTo(Point3D point)
 	{
 		long dx, dy;
-		synchronized (point)
-		{
-			dx = _x - point._x;
-			dy = _y - point._y;
-		}
+		dx = _x - point._x;
+		dy = _y - point._y;
 		return (dx * dx) + (dy * dy);
 	}
 	
 	public static long distanceSquared(Point3D point1, Point3D point2)
 	{
 		long dx, dy;
-		synchronized (point1)
-		{
-			synchronized (point2)
-			{
-				dx = point1._x - point2._x;
-				dy = point1._y - point2._y;
-			}
-		}
+		dx = point1._x - point2._x;
+		dy = point1._y - point2._y;
 		return (dx * dx) + (dy * dy);
 	}
 	
@@ -141,40 +123,44 @@ public class Point3D implements Serializable
 		return distanceSquared(point1, point2) < distance * distance;
 	}
 	
-	public synchronized int getX()
+	public int getX()
 	{
 		return _x;
 	}
 	
-	public synchronized void setX(int pX)
+	public void setX(int pX)
 	{
 		_x = pX;
 	}
 	
-	public synchronized int getY()
+	public int getY()
 	{
 		return _y;
 	}
 	
-	public synchronized void setY(int pY)
+	public void setY(int pY)
 	{
 		_y = pY;
 	}
 	
-	public synchronized int getZ()
+	public int getZ()
 	{
 		return _z;
 	}
 	
-	public synchronized void setZ(int pZ)
+	public void setZ(int pZ)
 	{
 		_z = pZ;
 	}
 	
-	public synchronized void setXYZ(int pX, int pY, int pZ)
+	public void setXYZ(int pX, int pY, int pZ)
 	{
 		_x = pX;
 		_y = pY;
 		_z = pZ;
 	}
+	
+	/* note by Deedlit: we are using volatile variable types here. We dont need to additionally 
+	 * use synchronized, cause volatile vars are synced vars. Removed all, please test if all is ok. Need to
+	 * look through code and remove similar pointless lockings if any more there. */
 }
