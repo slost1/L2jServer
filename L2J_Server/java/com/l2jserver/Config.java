fbd2bf0d28f550042121ea24c62b45f4af69cd02
MELERIX <MELERIX@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2011-07-01 22:41:55 +0000

    BETA/STABLE: Updating default values for AugmentationBlackList.
    git-svn-id: http://svn.l2jserver.com/trunk@4704 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2J_Server/java/com/l2jserver/Config.java
+++ L2J_Server/java/com/l2jserver/Config.java
@@ -1573,7 +1573,7 @@ public final class Config
 					AUGMENTATION_BASESTAT_CHANCE = Integer.parseInt(Character.getProperty("AugmentationBaseStatChance", "1"));
 					AUGMENTATION_ACC_SKILL_CHANCE = Integer.parseInt(Character.getProperty("AugmentationAccSkillChance", "0"));
 					
-					String[] array = Character.getProperty("AugmentationBlackList", "6656,6657,6658,6659,6660,6661,6662,8191,10170,10314").split(",");
+					String[] array = Character.getProperty("AugmentationBlackList", "6656,6657,6658,6659,6660,6661,6662,8191,10170,10314,13740,13741,13742,13743,13744,13745,13746,13747,13748,14592,14593,14594,14595,14596,14597,14598,14599,14600,14664,14665,14666,14667,14668,14669,14670,14671,14672,14801,14802,14803,14804,14805,14806,14807,14808,14809,15282,15283,15284,15285,15286,15287,15288,15289,15290,15291,15292,15293,15294,15295,15296,15297,15298,15299,16025,16026").split(",");
 					AUGMENTATION_BLACKLIST = new int[array.length];
 					
 					for (int i = 0; i < array.length; i++)
