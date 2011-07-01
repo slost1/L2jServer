6a8a20e5e607ee780e132e1cd6c123a6b11c16ff
MELERIX <MELERIX@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2011-07-01 22:41:55 +0000

    BETA/STABLE: Updating default values for AugmentationBlackList.
    git-svn-id: http://svn.l2jserver.com/branches/unstable@4704 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2J_Server_BETA/dist/startGameServer.bat
+++ L2J_Server_BETA/dist/startGameServer.bat
@@ -9,9 +9,6 @@ java -Djava.util.logging.manager=com.l2jserver.util.L2LogManager -Xms1024m -Xmx1
 REM
 REM If you have a big server and lots of memory, you could experiment for example with
 REM java -server -Xmx1536m -Xms1024m -Xmn512m -XX:PermSize=256m -XX:SurvivorRatio=8 -Xnoclassgc -XX:+AggressiveOpts
-REM If you are having troubles on server shutdown (saving data),
-REM add this to startup paramethers: -Djava.util.logging.manager=com.l2jserver.L2LogManager. Example:
-REM java -Djava.util.logging.manager=com.l2jserver.util.L2LogManager -Xmx1024m -cp ./../libs/*;l2jserver.jar com.l2jserver.gameserver.GameServer
 REM -------------------------------------
 if ERRORLEVEL 2 goto restart
 if ERRORLEVEL 1 goto error
--- L2J_Server_BETA/dist/startGameServer.ps1
+++ L2J_Server_BETA/dist/startGameServer.ps1
@@ -16,9 +16,6 @@ do
     #
     # If you have a big server and lots of memory, you could experiment for example with
     # java -server -Xmx1536m -Xms1024m -Xmn512m -XX:PermSize=256m -XX:SurvivorRatio=8 -Xnoclassgc -XX:+AggressiveOpts
-    # If you are having troubles on server shutdown (saving data),
-    # add this to startup paramethers: "-Djava.util.logging.manager=com.l2jserver.L2LogManager". Example:
-    # REM java "-Djava.util.logging.manager=com.l2jserver.util.L2LogManager" -Xmx1024m -cp "./../libs/*;l2jserver.jar" com.l2jserver.gameserver.GameServer
     # -------------------------------------
 }
 while ($LASTEXITCODE -like 2)
