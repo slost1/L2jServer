56cc23a8ad1378d5b9938dd075c1383282a95aa4
Gnacik <Gnacik@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2011-07-10 07:59:44 +0000

    STABLE: Fix for shifting items with long names eg. '''+4 Augumented Tallum Blade*Dark Legion's Edge - Lighting'''
    git-svn-id: http://svn.l2jserver.com/trunk@4722 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2J_Server/java/com/l2jserver/gameserver/network/clientpackets/Say2.java
+++ L2J_Server/java/com/l2jserver/gameserver/network/clientpackets/Say2.java
@@ -135,7 +135,8 @@ public final class Say2 extends L2GameClientPacket
 		
 		// Even though the client can handle more characters than it's current limit allows, an overflow (critical error) happens if you pass a huge (1000+) message.
 		// April 27, 2009 - Verified on Gracia P2 & Final official client as 105
-		if (_text.length() > 105 && !activeChar.isGM())
+		// Allow higher limit if player shift some item (text is longer then)
+		if (!activeChar.isGM() && ((_text.indexOf(8) >= 0 && _text.length() > 500) || (_text.indexOf(8) < 0 && _text.length() > 105)))
 		{
 			activeChar.sendPacket(SystemMessage.getSystemMessage(SystemMessageId.DONT_SPAM));
 			return;
