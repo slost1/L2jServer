15c4765e59073f7d2f45fc24628fd28f6f1240a5
UnAfraid <UnAfraid@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2011-09-25 23:43:21 +0000

    git-svn-id: http://svn.l2jserver.com/branches/unstable@4932 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2J_Server_BETA/java/com/l2jserver/gameserver/model/actor/instance/L2PcInstance.java
+++ L2J_Server_BETA/java/com/l2jserver/gameserver/model/actor/instance/L2PcInstance.java
@@ -666,7 +666,7 @@ public final class L2PcInstance extends L2Playable
 	private boolean _messageRefusal = false;    // message refusal mode
 	
 	private boolean _silenceMode = false;     // silence mode
-	private TIntArrayList _silenceModeExcluded = new TIntArrayList();     // silence mode
+	private final TIntArrayList _silenceModeExcluded = new TIntArrayList();     // silence mode
 	private boolean _dietMode = false;          // ignore weight penalty
 	private boolean _tradeRefusal = false;       // Trade refusal
 	private boolean _exchangeRefusal = false;   // Exchange refusal
@@ -14472,6 +14472,8 @@ public final class L2PcInstance extends L2Playable
 	
 	/**
 	 * While at silenceMode, checks if this PC Instance blocks PMs for this user
+	 * @param objId 
+	 * @return 
 	 */
 	public boolean isSilenceMode(int objId)
 	{
