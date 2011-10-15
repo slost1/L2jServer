1eb03b0ce887c3fcaa126b2ae871454edc22e87c
Zoey76 <Zoey76@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2011-10-15 01:55:56 +0000

    BETA: Typo fix for [5005], and some JavaDocs.
    git-svn-id: http://svn.l2jserver.com/branches/unstable@5009 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2J_Server_BETA/java/com/l2jserver/gameserver/templates/item/L2Item.java
+++ L2J_Server_BETA/java/com/l2jserver/gameserver/templates/item/L2Item.java
@@ -416,7 +416,7 @@ public abstract class L2Item
 	/**
 	 * Returns the grade of the item.<BR><BR>
 	 * <U><I>Concept :</I></U><BR>
-	 * In fact, this fucntion returns the type of crystal of the item.
+	 * In fact, this function returns the type of crystal of the item.
 	 * @return int
 	 */
 	public final int getItemGrade()
@@ -425,9 +425,8 @@ public abstract class L2Item
 	}
 	
 	/**
-	 * Returns the grade of the item.<BR><BR>
 	 * For grades S80 and S84 return S
-	 * @return int
+	 * @return the grade of the item.
 	 */
 	public final int getItemGradeSPlus()
 	{
@@ -442,8 +441,7 @@ public abstract class L2Item
 	}
 	
 	/**
-	 * Returns the quantity of crystals for crystallization
-	 * @return int
+	 * @return the quantity of crystals for crystallization.
 	 */
 	public final int getCrystalCount()
 	{
@@ -483,8 +481,7 @@ public abstract class L2Item
 	}
 	
 	/**
-	 * Returns the name of the item
-	 * @return String
+	 * @return the name of the item.
 	 */
 	public final String getName()
 	{
@@ -492,8 +489,7 @@ public abstract class L2Item
 	}
 	
 	/**
-	 * Returns the base elemental of the item
-	 * @return Elementals
+	 * @return the base elemental of the item.
 	 */
 	public final Elementals[] getElementals()
 	{
@@ -511,8 +507,8 @@ public abstract class L2Item
 	}
 	
 	/**
-	 * Sets the base elemental of the item
-	 * @param element 
+	 * Sets the base elemental of the item.
+	 * @param element the element to set.
 	 */
 	public void setElementals(Elementals element)
 	{
@@ -540,16 +536,14 @@ public abstract class L2Item
 	}
 	
 	/**
-	 * Return the part of the body used with the item.
-	 * @return int
+	 * @return the part of the body used with the item.
 	 */
 	public final int getBodyPart()
 	{
 		return _bodyPart;
 	}	
 	/**
-	 * Returns the type 1 of the item
-	 * @return int
+	 * @return the type 1 of the item.
 	 */
 	public final int getType1()
 	{
@@ -557,8 +551,7 @@ public abstract class L2Item
 	}
 	
 	/**
-	 * Returns if the item is stackable
-	 * @return boolean
+	 * @return {@code true} if the item is stackable, {@code false} otherwise.
 	 */
 	public final boolean isStackable()
 	{
@@ -566,22 +559,23 @@ public abstract class L2Item
 	}
 	
 	/**
-	 * Returns if the item is consumable
-	 * @return boolean
+	 * @return {@code true} if the item is consumable, {@code false} otherwise.
 	 */
 	public boolean isConsumable()
 	{
 		return false;
 	}
 	
+	/**
+	 * @return {@code true} if the item can be equipped, {@code false} otherwise.
+	 */
 	public boolean isEquipable()
 	{
-		return this.getBodyPart() != 0 && !(this.getItemType() instanceof L2EtcItemType);
+		return getBodyPart() != 0 && !(getItemType() instanceof L2EtcItemType);
 	}
 	
 	/**
-	 * Returns the price of reference of the item
-	 * @return int
+	 * @return the price of reference of the item.
 	 */
 	public final int getReferencePrice()
 	{
@@ -589,8 +583,7 @@ public abstract class L2Item
 	}
 	
 	/**
-	 * Returns if the item can be sold
-	 * @return boolean
+	 * @return {@code true} if the item can be sold, {@code false} otherwise.
 	 */
 	public final boolean isSellable()
 	{
@@ -598,8 +591,7 @@ public abstract class L2Item
 	}
 	
 	/**
-	 * Returns if the item can dropped
-	 * @return boolean
+	 * @return {@code true} if the item can be dropped, {@code false} otherwise.
 	 */
 	public final boolean isDropable()
 	{
@@ -607,8 +599,7 @@ public abstract class L2Item
 	}
 	
 	/**
-	 * Returns if the item can destroy
-	 * @return boolean
+	 * @return {@code true} if the item can be destroyed, {@code false} otherwise.
 	 */
 	public final boolean isDestroyable()
 	{
@@ -616,8 +607,7 @@ public abstract class L2Item
 	}
 	
 	/**
-	 * Returns if the item can add to trade
-	 * @return boolean
+	 * @return {@code true} if the item can be traded, {@code false} otherwise.
 	 */
 	public final boolean isTradeable()
 	{
@@ -625,8 +615,7 @@ public abstract class L2Item
 	}
 	
 	/**
-	 * Returns if the item can be put into warehouse
-	 * @return boolean
+	 * @return {@code true} if the item can be put into warehouse, {@code false} otherwise.
 	 */
 	public final boolean isDepositable()
 	{
@@ -634,11 +623,12 @@ public abstract class L2Item
 	}
 	
 	/**
+	 * This method also check the enchant blacklist.
 	 * @return {@code true} if the item can be enchanted, {@code false} otherwise.
 	 */
 	public final boolean isEnchantable()
 	{
-		return _enchantable && Arrays.binarySearch(Config.ENCHANT_BLACKLIST, getItemId()) == 0;
+		return _enchantable && Arrays.binarySearch(Config.ENCHANT_BLACKLIST, getItemId()) < 0;
 	}
 	
 	/**
