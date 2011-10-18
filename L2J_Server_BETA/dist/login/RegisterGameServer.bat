a501f42adf9a81ce68113aa970adf44b7ab3de68
UnAfraid <UnAfraid@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2011-10-18 21:42:31 +0000

    BETA: Moving all tools inside com.l2jserver.tools
    git-svn-id: http://svn.l2jserver.com/branches/unstable@5019 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2J_Server_BETA/dist/login/RegisterGameServer.bat
+++ L2J_Server_BETA/dist/login/RegisterGameServer.bat
@@ -1,5 +1,5 @@
 @echo off
 color 17
 cls
-java -Djava.util.logging.config.file=console.cfg -cp ./../libs/*;l2jlogin.jar com.l2jserver.gsregistering.BaseGameServerRegister -c
+java -Djava.util.logging.config.file=console.cfg -cp ./../libs/*;l2jlogin.jar com.l2jserver.tools.gsregistering.BaseGameServerRegister -c
 exit
