94cc2ac5a0e77c7fb7b4c7311ad65badd47ff4af
MELERIX <MELERIX@d34dfa0f-f91c-0410-86dd-cf06f19dd0ad>
2011-10-25 23:50:53 +0000

    BETA/STABLE: Updating Respawn Time for the old Zaken.
    git-svn-id: http://svn.l2jserver.com/trunk@5041 d34dfa0f-f91c-0410-86dd-cf06f19dd0ad

--- L2J_Server/java/com/l2jserver/Config.java
+++ L2J_Server/java/com/l2jserver/Config.java
@@ -2756,77 +2756,77 @@ public final class Config
 					
 					Interval_Of_Antharas_Spawn = Integer.parseInt(grandbossSettings.getProperty("IntervalOfAntharasSpawn", "264"));
 					if (Interval_Of_Antharas_Spawn < 1 || Interval_Of_Antharas_Spawn > 480)
-						Interval_Of_Antharas_Spawn = 192;
+						Interval_Of_Antharas_Spawn = 264;
 					Interval_Of_Antharas_Spawn = Interval_Of_Antharas_Spawn * 3600000;
 
 					Random_Of_Antharas_Spawn = Integer.parseInt(grandbossSettings.getProperty("RandomOfAntharasSpawn", "72"));
 					if (Random_Of_Antharas_Spawn < 1 || Random_Of_Antharas_Spawn > 192)
-						Random_Of_Antharas_Spawn = 145;
+						Random_Of_Antharas_Spawn = 72;
 					Random_Of_Antharas_Spawn = Random_Of_Antharas_Spawn * 3600000;
 					
 					Interval_Of_Valakas_Spawn = Integer.parseInt(grandbossSettings.getProperty("IntervalOfValakasSpawn", "264"));
 					if (Interval_Of_Valakas_Spawn < 1 || Interval_Of_Valakas_Spawn > 480)
-						Interval_Of_Valakas_Spawn = 192;
+						Interval_Of_Valakas_Spawn = 264;
 					Interval_Of_Valakas_Spawn = Interval_Of_Valakas_Spawn * 3600000;
 					
 					Random_Of_Valakas_Spawn = Integer.parseInt(grandbossSettings.getProperty("RandomOfValakasSpawn", "72"));
 					if (Random_Of_Valakas_Spawn < 1 || Random_Of_Valakas_Spawn > 192)
-						Random_Of_Valakas_Spawn = 145;
+						Random_Of_Valakas_Spawn = 72;
 					Random_Of_Valakas_Spawn = Random_Of_Valakas_Spawn * 3600000;
 					
 					Interval_Of_Baium_Spawn = Integer.parseInt(grandbossSettings.getProperty("IntervalOfBaiumSpawn", "168"));
 					if (Interval_Of_Baium_Spawn < 1 || Interval_Of_Baium_Spawn > 480)
-						Interval_Of_Baium_Spawn = 121;
+						Interval_Of_Baium_Spawn = 168;
 					Interval_Of_Baium_Spawn = Interval_Of_Baium_Spawn * 3600000;
 					
 					Random_Of_Baium_Spawn = Integer.parseInt(grandbossSettings.getProperty("RandomOfBaiumSpawn", "48"));
 					if (Random_Of_Baium_Spawn < 1 || Random_Of_Baium_Spawn > 192)
-						Random_Of_Baium_Spawn = 8;
+						Random_Of_Baium_Spawn = 48;
 					Random_Of_Baium_Spawn = Random_Of_Baium_Spawn * 3600000;
 					
 					Interval_Of_Core_Spawn = Integer.parseInt(grandbossSettings.getProperty("IntervalOfCoreSpawn", "60"));
 					if (Interval_Of_Core_Spawn < 1 || Interval_Of_Core_Spawn > 480)
-						Interval_Of_Core_Spawn = 27;
+						Interval_Of_Core_Spawn = 60;
 					Interval_Of_Core_Spawn = Interval_Of_Core_Spawn * 3600000;
 					
 					Random_Of_Core_Spawn = Integer.parseInt(grandbossSettings.getProperty("RandomOfCoreSpawn", "24"));
 					if (Random_Of_Core_Spawn < 1 || Random_Of_Core_Spawn > 192)
-						Random_Of_Core_Spawn = 47;
+						Random_Of_Core_Spawn = 24;
 					Random_Of_Core_Spawn = Random_Of_Core_Spawn * 3600000;
 					
 					Interval_Of_Orfen_Spawn = Integer.parseInt(grandbossSettings.getProperty("IntervalOfOrfenSpawn", "48"));
 					if (Interval_Of_Orfen_Spawn < 1 || Interval_Of_Orfen_Spawn > 480)
-						Interval_Of_Orfen_Spawn = 28;
+						Interval_Of_Orfen_Spawn = 48;
 					Interval_Of_Orfen_Spawn = Interval_Of_Orfen_Spawn * 3600000;
 					
 					Random_Of_Orfen_Spawn = Integer.parseInt(grandbossSettings.getProperty("RandomOfOrfenSpawn", "20"));
 					if (Random_Of_Orfen_Spawn < 1 || Random_Of_Orfen_Spawn > 192)
-						Random_Of_Orfen_Spawn = 41;
+						Random_Of_Orfen_Spawn = 20;
 					Random_Of_Orfen_Spawn = Random_Of_Orfen_Spawn * 3600000;
 					
 					Interval_Of_QueenAnt_Spawn = Integer.parseInt(grandbossSettings.getProperty("IntervalOfQueenAntSpawn", "36"));
 					if (Interval_Of_QueenAnt_Spawn < 1 || Interval_Of_QueenAnt_Spawn > 480)
-						Interval_Of_QueenAnt_Spawn = 19;
+						Interval_Of_QueenAnt_Spawn = 36;
 					Interval_Of_QueenAnt_Spawn = Interval_Of_QueenAnt_Spawn * 3600000;
 					
 					Random_Of_QueenAnt_Spawn = Integer.parseInt(grandbossSettings.getProperty("RandomOfQueenAntSpawn", "17"));
 					if (Random_Of_QueenAnt_Spawn < 1 || Random_Of_QueenAnt_Spawn > 192)
-						Random_Of_QueenAnt_Spawn = 35;
+						Random_Of_QueenAnt_Spawn = 17;
 					Random_Of_QueenAnt_Spawn = Random_Of_QueenAnt_Spawn * 3600000;
 					
-					Interval_Of_Zaken_Spawn = Integer.parseInt(grandbossSettings.getProperty("IntervalOfZakenSpawn", "19"));
+					Interval_Of_Zaken_Spawn = Integer.parseInt(grandbossSettings.getProperty("IntervalOfZakenSpawn", "60"));
 					if (Interval_Of_Zaken_Spawn < 1 || Interval_Of_Zaken_Spawn > 480)
-						Interval_Of_Zaken_Spawn = 19;
+						Interval_Of_Zaken_Spawn = 60;
 					Interval_Of_Zaken_Spawn = Interval_Of_Zaken_Spawn * 3600000;
 					
-					Random_Of_Zaken_Spawn = Integer.parseInt(grandbossSettings.getProperty("RandomOfZakenSpawn", "35"));
+					Random_Of_Zaken_Spawn = Integer.parseInt(grandbossSettings.getProperty("RandomOfZakenSpawn", "20"));
 					if (Random_Of_Zaken_Spawn < 1 || Random_Of_Zaken_Spawn > 192)
-						Random_Of_Zaken_Spawn = 35;
+						Random_Of_Zaken_Spawn = 20;
 					Random_Of_Zaken_Spawn = Random_Of_Zaken_Spawn * 3600000;
 					
 					Interval_Of_Frintezza_Spawn = Integer.parseInt(grandbossSettings.getProperty("IntervalOfFrintezzaSpawn", "48"));
 					if (Interval_Of_Frintezza_Spawn < 1 || Interval_Of_Frintezza_Spawn > 480)
-						Interval_Of_Frintezza_Spawn = 121;
+						Interval_Of_Frintezza_Spawn = 48;
 					Interval_Of_Frintezza_Spawn = Interval_Of_Frintezza_Spawn * 3600000;
 					
 					Random_Of_Frintezza_Spawn = Integer.parseInt(grandbossSettings.getProperty("RandomOfFrintezzaSpawn", "8"));
